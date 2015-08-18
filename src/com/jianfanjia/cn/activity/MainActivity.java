ea5a694c5330ce0176c4fe8b6be2660a2f5bc214
feng0413 <041332feng@163.com>
2015-08-18 15:01:00 +0800

    commit

--- src/com/jianfanjia/cn/activity/MainActivity.java
+++ src/com/jianfanjia/cn/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.jianfanjia.cn.activity;
 
+import android.os.Bundle;
+import android.util.Log;
 import com.jianfanjia.cn.base.BaseActivity;
 
 /**
@@ -13,6 +15,12 @@ import com.jianfanjia.cn.base.BaseActivity;
 public class MainActivity extends BaseActivity {
 
 	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// PushManager.getInstance().initialize(getApplicationContext());
+	}
+
+	@Override
 	public void initView() {
 		// TODO Auto-generated method stub
 
@@ -22,6 +30,14 @@ public class MainActivity extends BaseActivity {
 	public void setListener() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d(this.getClass().getName(), "---onDestroy()");
+		// PushManager.getInstance().stopService(getApplicationContext());//
+		// 完全终止SDK的服务
 	}
 
 	@Override
