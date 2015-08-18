4d1078e87867c2d6496678879c2b3dcf915e65b2
feng0413 <041332feng@163.com>
2015-08-18 15:09:57 +0800

    commit

---
+++ src/com/jianfanjia/cn/db/DBHelper.java
@@ -0,0 +1,13 @@
+package com.jianfanjia.cn.db;
+
+/**
+ * 
+ * @ClassName: DBHelper
+ * @Description: Êı¾İ¿â
+ * @author fengliang
+ * @date 2015-8-18 ÏÂÎç3:09:13
+ * 
+ */
+public class DBHelper {
+
+}
