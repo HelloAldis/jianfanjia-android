a43630f121d1eeb5f2a76c52431e3af7ad1b87fa
feng0413 <041332feng@163.com>
2015-08-18 15:07:02 +0800

    commit

---
+++ src/com/jianfanjia/cn/tools/MyCountDownTimer.java
@@ -0,0 +1,35 @@
+package com.jianfanjia.cn.tools;
+
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+/**
+ * 
+ * @ClassName: MyCountDownTimer
+ * @Description: 倒计时器
+ * @author fengliang
+ * @date 2015-8-18 下午3:04:15
+ * 
+ */
+public class MyCountDownTimer extends CountDownTimer {
+	private TextView textView;
+
+	public MyCountDownTimer(long millisInFuture, long countDownInterval,
+			TextView textView) {
+		super(millisInFuture, countDownInterval);
+		this.textView = textView;
+	}
+
+	@Override
+	public void onFinish() {
+		textView.setText("重获验证码");
+		textView.setEnabled(true);
+	}
+
+	@Override
+	public void onTick(long millisUntilFinished) {
+		textView.setEnabled(false);
+		textView.setText("(" + millisUntilFinished / 1000 + "s)重新获取");
+	}
+
+}
