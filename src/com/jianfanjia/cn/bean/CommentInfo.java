c558a8c9f175cdf4683361b8d7a07d11a312e471
feng0413 <041332feng@163.com>
2015-08-25 16:40:18 +0800

    commit

--- src/com/jianfanjia/cn/bean/CommentInfo.java
+++ src/com/jianfanjia/cn/bean/CommentInfo.java
@@ -1,5 +1,16 @@
 package com.jianfanjia.cn.bean;
 
-public class CommentInfo {
+import java.io.Serializable;
+
+/**
+ * 
+ * @ClassName: CommentInfo
+ * @Description: ÆÀÂÛ
+ * @author fengliang
+ * @date 2015-8-25 ÉÏÎç11:34:20
+ * 
+ */
+public class CommentInfo implements Serializable {
+	private static final long serialVersionUID = 6340039239493931968L;
 
 }
