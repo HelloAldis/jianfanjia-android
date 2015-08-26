091af79391b306c613050686ed74b8e70b0dea3b
zhhao226 <352233265@qq.com>
2015-08-26 11:14:16 +0800

    添加了资源文件

--- src/com/jianfanjia/cn/view/MainSiteLayout.java
+++ src/com/jianfanjia/cn/view/MainSiteLayout.java
@@ -4,7 +4,6 @@ import com.jianfanjia.cn.activity.R;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
