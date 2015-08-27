19ba1d26d0df5dff31107574de192d85b955c537
zhhao226 <352233265@qq.com>
2015-08-27 10:05:16 +0800

    commit

---
+++ src/com/jianfanjia/cn/activity/CommentActivity.java
@@ -0,0 +1,25 @@
+package com.jianfanjia.cn.activity;
+
+import com.jianfanjia.cn.base.BaseActivity;
+
+public class CommentActivity extends BaseActivity {
+
+	@Override
+	public int getLayoutId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void initView() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setListener() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
