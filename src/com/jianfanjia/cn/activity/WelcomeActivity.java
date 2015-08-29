73c9125b4eb352a327e293ca90843f9b7fb25b32
feng0413 <041332feng@163.com>
2015-08-29 09:37:25 +0800

    commit

---
+++ src/com/jianfanjia/cn/activity/WelcomeActivity.java
@@ -0,0 +1,57 @@
+package com.jianfanjia.cn.activity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import com.jianfanjia.cn.base.BaseActivity;
+
+/**
+ * 
+ * @ClassName: WelcomeActivity
+ * @Description: »¶Ó­
+ * @author fengliang
+ * @date 2015-8-29 ÉÏÎç9:30:21
+ * 
+ */
+public class WelcomeActivity extends BaseActivity {
+	private Handler handler = new Handler();
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	}
+
+	@Override
+	public void initView() {
+		handler.postDelayed(runnable, 3000);
+	}
+
+	@Override
+	public void setListener() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private Runnable runnable = new Runnable() {
+
+		@Override
+		public void run() {
+			startActivity(LoginActivity.class);
+			finish();
+		}
+
+	};
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		handler.removeCallbacks(runnable);
+	}
+
+	@Override
+	public int getLayoutId() {
+		return R.layout.activity_welcome;
+	}
+
+}
