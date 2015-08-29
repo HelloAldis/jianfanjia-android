c62f4c4fee7663333df9b065d1d96bdca3baf1f4
feng0413 <041332feng@163.com>
2015-08-29 11:52:58 +0800

    commit

--- src/com/jianfanjia/cn/activity/WelcomeActivity.java
+++ src/com/jianfanjia/cn/activity/WelcomeActivity.java
@@ -24,7 +24,7 @@ public class WelcomeActivity extends BaseActivity {
 
 	@Override
 	public void initView() {
-		handler.postDelayed(runnable, 3000);
+		handler.postDelayed(runnable, 2000);
 	}
 
 	@Override
