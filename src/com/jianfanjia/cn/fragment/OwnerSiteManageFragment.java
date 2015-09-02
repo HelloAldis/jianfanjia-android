ee57d73a00da8683746549e809972c215305a41c
feng0413 <041332feng@163.com>
2015-09-02 11:22:27 +0800

    commit

--- src/com/jianfanjia/cn/fragment/OwnerSiteManageFragment.java
+++ src/com/jianfanjia/cn/fragment/OwnerSiteManageFragment.java
@@ -38,7 +38,6 @@ import com.jianfanjia.cn.tools.DateFormatTool;
 import com.jianfanjia.cn.tools.JsonParser;
 import com.jianfanjia.cn.tools.LogTool;
 import com.jianfanjia.cn.tools.NetTool;
-import com.jianfanjia.cn.tools.SharedPrefer;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 /**
@@ -126,8 +125,9 @@ public class OwnerSiteManageFragment extends BaseFragment implements
 								CacheManager
 										.saveObject(getActivity(), processInfo,
 												Constant.PROCESSINFO_CACHE);
-								//保存业主的设计师id
-								shared.setValue(Constant.FINAL_DESIGNER_ID,processInfo.getFinal_designerid());
+								// 保存业主的设计师id
+								shared.setValue(Constant.FINAL_DESIGNER_ID,
+										processInfo.getFinal_designerid());
 								handlerSuccess();
 							} else if (response.has(Constant.ERROR_MSG)) {
 								makeTextLong(response.get(Constant.ERROR_MSG)
