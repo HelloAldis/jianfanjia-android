3739e885aae8e4134861e412d7dad3f4044845ee
feng0413 <041332feng@163.com>
2015-09-02 14:53:54 +0800

    commit

--- src/com/jianfanjia/cn/http/JianFanJiaApiClient.java
+++ src/com/jianfanjia/cn/http/JianFanJiaApiClient.java
@@ -29,6 +29,28 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
  * 
  */
 public class JianFanJiaApiClient {
+	/**
+	 * 上传个推clientid
+	 * 
+	 * @param context
+	 * @param clientId
+	 * @param handler
+	 */
+	public static void uploadRegisterId(Context context, String account,
+			String clientId, AsyncHttpResponseHandler handler) {
+		JSONObject jsonParams = new JSONObject();
+		try {
+			jsonParams.put("account", account);
+			jsonParams.put("cid", clientId);
+			StringEntity entity = new StringEntity(jsonParams.toString());
+			HttpRestClient.post(context, Url.LOGIN_URL, entity,
+					"application/json", handler);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * @author zhanghao
--- src/com/jianfanjia/cn/receiver/PushMsgReceiver.java
+++ src/com/jianfanjia/cn/receiver/PushMsgReceiver.java
@@ -1,5 +1,7 @@
 package com.jianfanjia.cn.receiver;
 
+import org.apache.http.Header;
+import org.json.JSONObject;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,8 +9,12 @@ import android.os.Bundle;
 import com.igexin.sdk.PushConsts;
 import com.igexin.sdk.PushManager;
 import com.jianfanjia.cn.bean.Message;
+import com.jianfanjia.cn.config.Constant;
+import com.jianfanjia.cn.http.JianFanJiaApiClient;
 import com.jianfanjia.cn.inter.manager.ListenerManeger;
 import com.jianfanjia.cn.tools.LogTool;
+import com.jianfanjia.cn.tools.SharedPrefer;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 /**
  * 
@@ -20,11 +26,16 @@ import com.jianfanjia.cn.tools.LogTool;
  */
 public class PushMsgReceiver extends BroadcastReceiver {
 	private static final String TAG = "PushMsgReceiver";
-	private ListenerManeger listenerManeger;
+	private SharedPrefer sharedPrefer = null;
+	private ListenerManeger listenerManeger = null;
+	private String account = null;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		sharedPrefer = new SharedPrefer(context, Constant.SHARED_MAIN);
 		listenerManeger = ListenerManeger.getListenerManeger();
+		account = sharedPrefer.getValue(Constant.ACCOUNT, null);
+		LogTool.d(TAG, "account=" + account);
 		// -------------------------------------------------------------------
 		Bundle bundle = intent.getExtras();
 		LogTool.d(TAG, "onReceive() action=" + bundle.getInt("action"));
@@ -49,6 +60,9 @@ public class PushMsgReceiver extends BroadcastReceiver {
 			// 第三方应用需要将CID上传到第三方服务器，并且将当前用户帐号和CID进行关联，以便日后通过用户帐号查找CID进行消息推送
 			String cid = bundle.getString("clientid");
 			LogTool.d(TAG, "cid:" + cid);
+			if (null != cid) {
+				uploadClientID(context, "", cid);
+			}
 			break;
 		case PushConsts.THIRDPART_FEEDBACK:
 			/*
@@ -78,4 +92,39 @@ public class PushMsgReceiver extends BroadcastReceiver {
 
 	}
 
+	/**
+	 * 上传clientid
+	 * 
+	 * @param context
+	 * @param account
+	 * @param clientId
+	 */
+	private void uploadClientID(Context context, String account, String clientId) {
+		JianFanJiaApiClient.uploadRegisterId(context, account, clientId,
+				new JsonHttpResponseHandler() {
+					@Override
+					public void onStart() {
+						LogTool.d(TAG, "onStart()");
+					}
+
+					@Override
+					public void onSuccess(int statusCode, Header[] headers,
+							JSONObject response) {
+						LogTool.d(TAG, "JSONObject response:" + response);
+					}
+
+					@Override
+					public void onFailure(int statusCode, Header[] headers,
+							Throwable throwable, JSONObject errorResponse) {
+						LogTool.d(TAG,
+								"Throwable throwable:" + throwable.toString());
+					}
+
+					@Override
+					public void onFailure(int statusCode, Header[] headers,
+							String responseString, Throwable throwable) {
+						LogTool.d(TAG, "throwable:" + throwable);
+					};
+				});
+	}
 }
