1b800ee289ffc18b8d432372955e889950e35cfe
feng0413 <041332feng@163.com>
2015-09-06 16:04:36 +0800

    commit

--- src/com/jianfanjia/cn/activity/UserByDesignerInfoActivity.java
+++ src/com/jianfanjia/cn/activity/UserByDesignerInfoActivity.java
@@ -1,6 +1,15 @@
 package com.jianfanjia.cn.activity;
 
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
 import com.jianfanjia.cn.base.BaseActivity;
+import com.jianfanjia.cn.bean.UserByDesignerInfo;
+import com.jianfanjia.cn.config.Constant;
+import com.jianfanjia.cn.http.JianFanJiaApiClient;
+import com.jianfanjia.cn.tools.JsonParser;
+import com.jianfanjia.cn.tools.LogTool;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 /**
  * 
@@ -16,14 +25,61 @@ public class UserByDesignerInfoActivity extends BaseActivity {
 
 	@Override
 	public void initView() {
-		// TODO Auto-generated method stub
-
+		get_Designer_Info();
 	}
 
 	@Override
 	public void setListener() {
 		// TODO Auto-generated method stub
 
+	}
+
+	private void get_Designer_Info() {
+		JianFanJiaApiClient.get_Designer_Info(UserByDesignerInfoActivity.this,
+				new JsonHttpResponseHandler() {
+					@Override
+					public void onStart() {
+						LogTool.d(TAG, "onStart()");
+					}
+
+					@Override
+					public void onSuccess(int statusCode, Header[] headers,
+							JSONObject response) {
+						LogTool.d(TAG, "JSONObject response:" + response);
+						try {
+							if (response.has(Constant.DATA)) {
+								UserByDesignerInfo info = JsonParser
+										.jsonToBean(response.get(Constant.DATA)
+												.toString(),
+												UserByDesignerInfo.class);
+								if (null != info) {
+
+								}
+							} else if (response.has(Constant.ERROR_MSG)) {
+								makeTextLong(response.get(Constant.ERROR_MSG)
+										.toString());
+							}
+						} catch (JSONException e) {
+							e.printStackTrace();
+							makeTextLong(getString(R.string.tip_login_error_for_network));
+						}
+					}
+
+					@Override
+					public void onFailure(int statusCode, Header[] headers,
+							Throwable throwable, JSONObject errorResponse) {
+						LogTool.d(TAG,
+								"Throwable throwable:" + throwable.toString());
+						makeTextLong(getString(R.string.tip_login_error_for_network));
+					}
+
+					@Override
+					public void onFailure(int statusCode, Header[] headers,
+							String responseString, Throwable throwable) {
+						LogTool.d(TAG, "throwable:" + throwable);
+						makeTextLong(getString(R.string.tip_login_error_for_network));
+					};
+				});
 	}
 
 	@Override
