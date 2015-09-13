73e3ad1590e8624b2607c3b3d22e482536faed41
feng0413 <041332feng@163.com>
2015-09-13 14:28:33 +0800

    commit

---
+++ src/com/jianfanjia/cn/activity/HelpActivity.java
@@ -0,0 +1,105 @@
+package com.jianfanjia.cn.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import com.jianfanjia.cn.adapter.ViewPageAdapter;
+import com.jianfanjia.cn.base.BaseActivity;
+
+/**
+ * 
+ * @ClassName: HelpActivity
+ * @Description: 帮助
+ * @author fengliang
+ * @date 2015-9-13 下午2:15:12
+ * 
+ */
+public class HelpActivity extends BaseActivity {
+	private ViewPager viewPager;
+	private ViewGroup group = null;
+	private ImageView[] tips;
+	private List<View> bannerList = new ArrayList<View>();
+	private static final int IMG_ID[] = { R.drawable.navigate_1,
+			R.drawable.navigate_2, R.drawable.navigate_3, R.drawable.navigate_4 };
+
+	@Override
+	public void initView() {
+		viewPager = (ViewPager) findViewById(R.id.viewPager);
+		group = (ViewGroup) findViewById(R.id.viewGroup);
+		for (int i = 0; i < IMG_ID.length; i++) {
+			ImageView imageView = new ImageView(HelpActivity.this);
+			imageView.setBackgroundResource(IMG_ID[i]);
+			bannerList.add(imageView);
+		}
+		// 将点点加入到ViewGroup中
+		tips = new ImageView[bannerList.size()];
+		for (int i = 0; i < tips.length; i++) {
+			ImageView imageView = new ImageView(HelpActivity.this);
+			imageView.setLayoutParams(new LinearLayout.LayoutParams(10, 10));
+			tips[i] = imageView;
+			if (i == 0) {
+				tips[i].setBackgroundResource(R.drawable.new_gallery_dianpu_selected);
+			} else {
+				tips[i].setBackgroundResource(R.drawable.new_gallery_dianpu_normal);
+			}
+			LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+					new ViewGroup.LayoutParams(20, 20));
+			layoutParams.leftMargin = 15;
+			layoutParams.rightMargin = 15;
+			group.addView(imageView, layoutParams);
+		}
+		ViewPageAdapter pageAdapter = new ViewPageAdapter(HelpActivity.this,
+				bannerList);
+		viewPager.setOnPageChangeListener(new OnPageChangeListener() {
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onPageSelected(int arg0) {
+				setImageBackground(arg0 % bannerList.size());
+			}
+		});
+		viewPager.setAdapter(pageAdapter);
+		viewPager.setCurrentItem(0);
+	}
+
+	@Override
+	public void setListener() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * 设置选中的索引的背景
+	 * 
+	 * @param selectItems
+	 */
+	private void setImageBackground(int selectItems) {
+		for (int i = 0; i < tips.length; i++) {
+			if (i == selectItems) {
+				tips[i].setBackgroundResource(R.drawable.new_gallery_dianpu_selected);
+			} else {
+				tips[i].setBackgroundResource(R.drawable.new_gallery_dianpu_normal);
+			}
+		}
+	}
+
+	@Override
+	public int getLayoutId() {
+		return R.layout.activity_help;
+	}
+}
