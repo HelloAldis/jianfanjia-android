198240903fa0a72f4c5fec0f7829b47ce691d281
feng0413 <041332feng@163.com>
2015-09-14 13:10:44 +0800

    commit

--- src/com/jianfanjia/cn/cache/DataManager.java
+++ src/com/jianfanjia/cn/cache/DataManager.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Observable;
 import org.apache.http.Header;
 import org.json.JSONException;
 import org.json.JSONObject;
