6683bc3fca1676834998035d3ae9bac0643b63f3
feng0413 <041332feng@163.com>
2015-09-15 17:09:41 +0800

    commit

--- src/com/jianfanjia/cn/view/MainHeadView.java
+++ src/com/jianfanjia/cn/view/MainHeadView.java
@@ -6,90 +6,97 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import com.jianfanjia.cn.activity.R;
 import com.jianfanjia.cn.layout.CircleImageView;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
-public class MainHeadView extends RelativeLayout{
-	
+public class MainHeadView extends RelativeLayout {
 	private LayoutInflater inflater;
 	private View mainHead;
 	private RelativeLayout mainLayout;
 	private CircleImageView headImageView;
 	private TextView mainTitle;
 	private TextView rigthTitle;
-	private View divider;//分割线
+	private View divider;// 分割线
 
 	public MainHeadView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		inflater = LayoutInflater.from(context);
 		mainHead = inflater.inflate(R.layout.common_head, null);
 		mainLayout = (RelativeLayout) mainHead.findViewById(R.id.head_layout);
-		headImageView = (CircleImageView) mainLayout.findViewById(R.id.icon_head);
+		headImageView = (CircleImageView) mainLayout
+				.findViewById(R.id.icon_head);
 		mainTitle = (TextView) mainLayout.findViewById(R.id.head_center_title);
 		rigthTitle = (TextView) mainLayout.findViewById(R.id.head_right_title);
 		divider = mainLayout.findViewById(R.id.head_divier);
-		addView(mainHead,new LayoutParams(android.view.ViewGroup.LayoutParams.MATCH_PARENT,android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
+		addView(mainHead, new LayoutParams(
+				android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+				android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
 	}
 
 	public MainHeadView(Context context, AttributeSet attrs) {
-		this(context, attrs,0);
+		this(context, attrs, 0);
 		// TODO Auto-generated constructor stub
 	}
 
 	public MainHeadView(Context context) {
-		this(context,null,0);
+		this(context, null, 0);
 		// TODO Auto-generated constructor stub
 	}
-	
-	public void setMianTitle(String message){
-		if(message == null) return;
+
+	public void setMianTitle(String message) {
+		if (message == null)
+			return;
 		mainTitle.setText(message);
 	}
-	
-	public void setRightTitle(String message){
-		if(message == null)  return;
+
+	public void setRightTitle(String message) {
+		if (message == null)
+			return;
 		rigthTitle.setText(message);
 	}
-	
-	public void setBackgroundTransparent(){
+
+	public void setBackgroundTransparent() {
 		mainLayout.setBackgroundResource(R.color.transparent);
 	}
-	
+
 	@Override
-	public void setBackgroundColor(int resColor){
+	public void setBackgroundColor(int resColor) {
 		mainLayout.setBackgroundResource(resColor);
 	}
-	
-	public void setRightTitleVisable(int visibility){
-		if(visibility != View.GONE || visibility != View.VISIBLE) return;
+
+	public void setRightTitleVisable(int visibility) {
+		if (visibility != View.GONE || visibility != View.VISIBLE)
+			return;
 		rigthTitle.setVisibility(visibility);
 	}
-	
-	public void setDividerVisable(int visibility){
-		if(visibility != View.GONE || visibility != View.VISIBLE) return;
+
+	public void setDividerVisable(int visibility) {
+		if (visibility != View.GONE || visibility != View.VISIBLE)
+			return;
 		divider.setVisibility(visibility);
 	}
-	
-	public void setHeadImage(String imagePath){
-		if(imagePath == null) return;
+
+	public void setHeadImage(String imagePath) {
+		if (imagePath == null)
+			return;
 		ImageLoader.getInstance().displayImage(imagePath, headImageView);
 	}
-	
-	public void setBackListener(OnClickListener onClickListener){
-		if(onClickListener == null)  return;
+
+	public void setBackListener(OnClickListener onClickListener) {
+		if (onClickListener == null)
+			return;
 		headImageView.setOnClickListener(onClickListener);
 	}
-	
-	public void setRightTextListener(OnClickListener onClickListener){
-		if(onClickListener == null)  return;
+
+	public void setRightTextListener(OnClickListener onClickListener) {
+		if (onClickListener == null)
+			return;
 		rigthTitle.setOnClickListener(onClickListener);
 	}
-	
-	public void setMainTextColor(int resColor){
+
+	public void setMainTextColor(int resColor) {
 		mainTitle.setTextColor(resColor);
 	}
-	
-	
+
 }
