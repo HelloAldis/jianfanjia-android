9bcc7081de012b106f39ad9f12fafc79c1d597d6
feng0413 <041332feng@163.com>
2015-09-21 17:26:28 +0800

    commit

--- src/com/jianfanjia/cn/fragment/FuKuanNotifyFragment.java
+++ src/com/jianfanjia/cn/fragment/FuKuanNotifyFragment.java
@@ -1,5 +1,6 @@
 package com.jianfanjia.cn.fragment;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import android.view.View;
@@ -10,6 +11,7 @@ import com.jianfanjia.cn.activity.R;
 import com.jianfanjia.cn.adapter.PayNotifyAdapter;
 import com.jianfanjia.cn.base.BaseFragment;
 import com.jianfanjia.cn.bean.NotifyMessage;
+import com.jianfanjia.cn.config.Constant;
 import com.jianfanjia.cn.interf.SwitchFragmentListener;
 import com.jianfanjia.cn.tools.LogTool;
 
@@ -23,15 +25,15 @@ import com.jianfanjia.cn.tools.LogTool;
  */
 public class FuKuanNotifyFragment extends BaseFragment implements
 		SwitchFragmentListener, OnItemLongClickListener {
-	private ListView fukuanListView;
+	private ListView fukuanListView = null;
 	private List<NotifyMessage> payList = new ArrayList<NotifyMessage>();
 	private PayNotifyAdapter payAdapter = null;
 
 	@Override
 	public void initView(View view) {
 		fukuanListView = (ListView) view.findViewById(R.id.tip_pay__listview);
-		// payAdapter = new PayNotifyAdapter(getActivity(), payList);
-		// listView.setAdapter(payAdapter);
+		payAdapter = new PayNotifyAdapter(getActivity(), payList);
+		fukuanListView.setAdapter(payAdapter);
 	}
 
 	@Override
@@ -40,9 +42,20 @@ public class FuKuanNotifyFragment extends BaseFragment implements
 		if (isVisibleToUser) {
 			// fragment可见时加载数据
 			LogTool.d(this.getClass().getName(), "FuKuanNotifyFragment 可见");
+			initData();
 		} else {
 			// 不可见时不执行操作
 			LogTool.d(this.getClass().getName(), "FuKuanNotifyFragment 不可见");
+		}
+	}
+
+	private void initData() {
+		try {
+			payList = daoManager.listByType(Constant.FUKUAN_NOTIFY);
+			LogTool.d(this.getClass().getName(), "payList:" + payList);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
--- src/com/jianfanjia/cn/fragment/YanQiNotifyFragment.java
+++ src/com/jianfanjia/cn/fragment/YanQiNotifyFragment.java
@@ -1,15 +1,19 @@
 package com.jianfanjia.cn.fragment;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
+
 import com.jianfanjia.cn.activity.R;
 import com.jianfanjia.cn.adapter.DelayNotifyAdapter;
 import com.jianfanjia.cn.base.BaseFragment;
 import com.jianfanjia.cn.bean.NotifyMessage;
+import com.jianfanjia.cn.config.Constant;
 import com.jianfanjia.cn.interf.SwitchFragmentListener;
 import com.jianfanjia.cn.tools.LogTool;
 
@@ -23,7 +27,6 @@ import com.jianfanjia.cn.tools.LogTool;
  */
 public class YanQiNotifyFragment extends BaseFragment implements
 		SwitchFragmentListener, OnItemLongClickListener {
-	private static final String TAG = YanQiNotifyFragment.class.getName();
 	private ListView yanqiListView = null;
 	private List<NotifyMessage> delayList = new ArrayList<NotifyMessage>();
 	private NotifyMessage notifyMessage = null;
@@ -32,6 +35,8 @@ public class YanQiNotifyFragment extends BaseFragment implements
 	@Override
 	public void initView(View view) {
 		yanqiListView = (ListView) view.findViewById(R.id.tip_delay__listview);
+		delayAdapter = new DelayNotifyAdapter(getActivity(), delayList);
+		yanqiListView.setAdapter(delayAdapter);
 	}
 
 	@Override
@@ -40,9 +45,20 @@ public class YanQiNotifyFragment extends BaseFragment implements
 		if (isVisibleToUser) {
 			// fragment可见时加载数据
 			LogTool.d(this.getClass().getName(), "YanQiNotifyFragment 可见");
+			initData();
 		} else {
 			// 不可见时不执行操作
 			LogTool.d(this.getClass().getName(), "YanQiNotifyFragment 不可见");
+		}
+	}
+
+	private void initData() {
+		try {
+			delayList = daoManager.listByType(Constant.YANQI_NOTIFY);
+			LogTool.d(this.getClass().getName(), "delayList:" + delayList);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
