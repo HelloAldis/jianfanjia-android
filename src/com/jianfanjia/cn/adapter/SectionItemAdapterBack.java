e3fdd565250a513c77d978dcb6b4b2055c81ba10
feng0413 <041332feng@163.com>
2015-09-22 10:36:59 +0800

    commit

--- src/com/jianfanjia/cn/adapter/SectionItemAdapterBack.java
+++ src/com/jianfanjia/cn/adapter/SectionItemAdapterBack.java
@@ -2,7 +2,6 @@ package com.jianfanjia.cn.adapter;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,7 +15,6 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import com.jianfanjia.cn.activity.R;
 import com.jianfanjia.cn.application.MyApplication;
 import com.jianfanjia.cn.bean.CommentInfo;
