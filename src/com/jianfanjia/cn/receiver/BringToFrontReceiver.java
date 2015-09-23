17d2237467a6c3842cc53f9d7127390b96473613
feng0413 <041332feng@163.com>
2015-09-23 23:31:30 +0800

    commit

---
+++ src/com/jianfanjia/cn/receiver/BringToFrontReceiver.java
@@ -0,0 +1,15 @@
+package com.jianfanjia.cn.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BringToFrontReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
