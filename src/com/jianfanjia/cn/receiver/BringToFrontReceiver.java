0272d4e3ddff629ce85b99b9964234a7399612aa
feng0413 <041332feng@163.com>
2015-09-24 16:52:03 +0800

    commit

--- src/com/jianfanjia/cn/receiver/BringToFrontReceiver.java
+++ src/com/jianfanjia/cn/receiver/BringToFrontReceiver.java
@@ -4,6 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+/**
+ * 
+ * @ClassName: BringToFrontReceiver
+ * @Description: TODO
+ * @author fengliang
+ * @date 2015-9-24 обнГ4:51:50
+ * 
+ */
 public class BringToFrontReceiver extends BroadcastReceiver {
 
 	@Override
