418b11efcdc97ff20875e0f0f420b532b58edf2e
feng0413 <041332feng@163.com>
2015-09-24 14:45:55 +0800

    commit

--- src/com/jianfanjia/cn/adapter/SectionItemAdapterBack.java
+++ src/com/jianfanjia/cn/adapter/SectionItemAdapterBack.java
@@ -165,6 +165,8 @@ public class SectionItemAdapterBack extends BaseAdapter {
 				convertView = layoutInflater.inflate(
 						R.layout.site_listview_head, null);
 				viewHolderf = new ViewHolder2();
+				viewHolderf.finishStatusIcon = (ImageView) convertView
+						.findViewById(R.id.site_listview_item_status);
 				viewHolderf.smallcloseLayout = (RelativeLayout) convertView
 						.findViewById(R.id.site_listview_item_content_small);
 				viewHolderf.bigOpenLayout = (RelativeLayout) convertView
@@ -280,7 +282,7 @@ public class SectionItemAdapterBack extends BaseAdapter {
 			if (date != 0l) {
 				viewHolder.openUploadTime.setText(StringUtils
 						.covertLongToString(date));
-			}else{
+			} else {
 				viewHolder.openUploadTime.setText("");
 			}
 
@@ -379,7 +381,17 @@ public class SectionItemAdapterBack extends BaseAdapter {
 				viewHolderf.openCheck.setText(context
 						.getString(R.string.site_example_node_check));
 			}
-
+			int state = sectionInfo.getStatus();
+			switch (state) {
+			case Constant.FINISH:
+				viewHolderf.finishStatusIcon
+						.setImageResource(R.drawable.icon_home_finish);
+				break;
+			default:
+				viewHolderf.finishStatusIcon
+						.setImageResource(R.drawable.site_listview_item_notstart_circle);
+				break;
+			}
 			viewHolderf.openDelay.setOnClickListener(new OnClickListener() {
 
 				@Override
@@ -456,6 +468,7 @@ public class SectionItemAdapterBack extends BaseAdapter {
 		TextView openCheck;
 		TextView openTip;
 		TextView closeTip;
+		ImageView finishStatusIcon;
 	}
 
 	@Override
