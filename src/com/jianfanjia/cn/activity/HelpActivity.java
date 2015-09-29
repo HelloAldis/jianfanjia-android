0faf1d7113c659e48228571f7967f0a6f9107d91
feng0413 <041332feng@163.com>
2015-09-29 10:36:55 +0800

    commit

--- src/com/jianfanjia/cn/activity/HelpActivity.java
+++ src/com/jianfanjia/cn/activity/HelpActivity.java
@@ -2,13 +2,10 @@ package com.jianfanjia.cn.activity;
 
 import java.util.ArrayList;
 import java.util.List;
-import android.graphics.Bitmap;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import com.jianfanjia.cn.adapter.ViewPageAdapter;
 import com.jianfanjia.cn.base.BaseActivity;
 
@@ -22,39 +19,18 @@ import com.jianfanjia.cn.base.BaseActivity;
  */
 public class HelpActivity extends BaseActivity {
 	private static final String TAG = HelpActivity.class.getName();
-	private ViewPager viewPager;
-	private ViewGroup group = null;
-	private ImageView[] tips;
+	private ViewPager viewPager = null;
 	private List<View> bannerList = new ArrayList<View>();
 	private static final int IMG_ID[] = { R.drawable.p1, R.drawable.p2,
 			R.drawable.p3, R.drawable.p4 };
-	private Bitmap bitmap = null;
 
 	@Override
 	public void initView() {
 		viewPager = (ViewPager) findViewById(R.id.viewPager);
-		group = (ViewGroup) findViewById(R.id.viewGroup);
 		for (int i = 0; i < IMG_ID.length; i++) {
 			ImageView view = new ImageView(this);
 			view.setImageResource(IMG_ID[i]);
 			bannerList.add(view);
-		}
-		// 将点点加入到ViewGroup中
-		tips = new ImageView[bannerList.size()];
-		for (int i = 0; i < tips.length; i++) {
-			ImageView imageView = new ImageView(HelpActivity.this);
-			imageView.setLayoutParams(new LinearLayout.LayoutParams(10, 10));
-			tips[i] = imageView;
-			if (i == 0) {
-				tips[i].setBackgroundResource(R.drawable.new_gallery_dianpu_selected);
-			} else {
-				tips[i].setBackgroundResource(R.drawable.new_gallery_dianpu_normal);
-			}
-			LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-					new ViewGroup.LayoutParams(20, 20));
-			layoutParams.leftMargin = 15;
-			layoutParams.rightMargin = 15;
-			group.addView(imageView, layoutParams);
 		}
 		ViewPageAdapter pageAdapter = new ViewPageAdapter(HelpActivity.this,
 				bannerList);
@@ -73,7 +49,7 @@ public class HelpActivity extends BaseActivity {
 
 			@Override
 			public void onPageSelected(int arg0) {
-				setImageBackground(arg0 % bannerList.size());
+
 			}
 		});
 		viewPager.setAdapter(pageAdapter);
@@ -84,21 +60,6 @@ public class HelpActivity extends BaseActivity {
 	public void setListener() {
 		// TODO Auto-generated method stub
 
-	}
-
-	/**
-	 * 设置选中的索引的背景
-	 * 
-	 * @param selectItems
-	 */
-	private void setImageBackground(int selectItems) {
-		for (int i = 0; i < tips.length; i++) {
-			if (i == selectItems) {
-				tips[i].setBackgroundResource(R.drawable.new_gallery_dianpu_selected);
-			} else {
-				tips[i].setBackgroundResource(R.drawable.new_gallery_dianpu_normal);
-			}
-		}
 	}
 
 	@Override
