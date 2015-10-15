99240d3537089866970950b7f9a83f2e0ab589fb
feng0413 <041332feng@163.com>
2015-10-15 13:05:25 +0800

    commit  -fragment

---
+++ app/src/main/java/com/jianfanjia/cn/activity/DesignerCaseInfoActivity.java
@@ -0,0 +1,27 @@
+package com.jianfanjia.cn.activity;
+
+import com.jianfanjia.cn.base.BaseActivity;
+
+/**
+ * Description:设计师案例详情
+ * Author：fengliang
+ * Email：leo.feng@myjyz.com
+ * Date:15-10-11 14:30
+ */
+public class DesignerCaseInfoActivity extends BaseActivity {
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_designer_case_info;
+    }
+}
