59c92668893e4c87e89deabfe7602d6941723645
feng0413 <041332feng@163.com>
2015-10-20 18:01:21 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/adapter/DesignerListAdapter.java
+++ app/src/main/java/com/jianfanjia/cn/adapter/DesignerListAdapter.java
@@ -55,13 +55,11 @@ public class DesignerListAdapter extends BaseListAdapter<DesignerListInfo> {
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
-
-
         Product product = info.getProduct();
         viewHolder.itemXiaoQuText.setText(product.getCell());
         String houseType = product.getHouse_type();
         String decStyle = product.getDec_style();
-        viewHolder.itemProduceText.setText(product.getHouse_area() + "㎡");
+        viewHolder.itemProduceText.setText(product.getHouse_area() + "㎡," + getHouseType(houseType) + "," + getDecStyle(decStyle));
         imageLoader.displayImage(Url_New.GET_IMAGE + product.getImages().get(0).getImageid(), viewHolder.itemProductView, options);
         imageLoader.displayImage(Url_New.GET_IMAGE + info.getImageid(), viewHolder.itemHeadView, options);
         viewHolder.itemProductView.setOnClickListener(new OnClickListener() {
@@ -88,4 +86,42 @@ public class DesignerListAdapter extends BaseListAdapter<DesignerListInfo> {
         TextView itemProduceText;
     }
 
+
+    private String getHouseType(String houseType) {
+        String str = null;
+        if (houseType.equals("0")) {
+            str = "一居";
+        } else if (houseType.equals("1")) {
+            str = "二居";
+        } else if (houseType.equals("2")) {
+            str = "三居";
+        } else if (houseType.equals("3")) {
+            str = "四居";
+        } else if (houseType.equals("4")) {
+            str = "复式";
+        } else if (houseType.equals("5")) {
+            str = "别墅";
+        } else if (houseType.equals("6")) {
+            str = "商业";
+        }
+        return str;
+    }
+
+    private String getDecStyle(String decStyle) {
+        String str = null;
+        if (decStyle.equals("0")) {
+            str = "欧式";
+        } else if (decStyle.equals("1")) {
+            str = "中式";
+        } else if (decStyle.equals("2")) {
+            str = "现代";
+        } else if (decStyle.equals("3")) {
+            str = "地中海";
+        } else if (decStyle.equals("4")) {
+            str = "美式";
+        } else if (decStyle.equals("5")) {
+            str = "东南亚";
+        }
+        return str;
+    }
 }
