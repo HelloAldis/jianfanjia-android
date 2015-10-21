a659a8a369a28e8342cafcddad2b8af5b69b64db
feng0413 <041332feng@163.com>
2015-10-21 08:43:21 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/fragment/DesignerInfoFragment.java
+++ app/src/main/java/com/jianfanjia/cn/fragment/DesignerInfoFragment.java
@@ -4,6 +4,9 @@ import android.view.View;
 
 import com.jianfanjia.cn.activity.R;
 import com.jianfanjia.cn.base.BaseFragment;
+import com.jianfanjia.cn.http.JianFanJiaClient;
+import com.jianfanjia.cn.interf.ApiUiUpdateListener;
+import com.jianfanjia.cn.tools.LogTool;
 
 /**
  * @author fengliang
@@ -11,16 +14,35 @@ import com.jianfanjia.cn.base.BaseFragment;
  * @Description: 设计师资料
  * @date 2015-8-26 下午1:07:52
  */
-public class DesignerInfoFragment extends BaseFragment {
-
+public class DesignerInfoFragment extends BaseFragment implements ApiUiUpdateListener {
+    private static final String TAG = DesignerInfoFragment.class.getName();
 
     @Override
     public void initView(View view) {
-
+        getDesignerProduct("55ebfc02d6e8f37706e4f1b7", 0, 5);
     }
 
     @Override
     public void setListener() {
+
+    }
+
+    private void getDesignerProduct(String designerid, int from, int limit) {
+        JianFanJiaClient.getDesignerProduct(getContext(), designerid, from, limit, this, this);
+    }
+
+    @Override
+    public void preLoad() {
+
+    }
+
+    @Override
+    public void loadSuccess(Object data) {
+        LogTool.d(TAG, "data:" + data);
+    }
+
+    @Override
+    public void loadFailture(String error_msg) {
 
     }
 
