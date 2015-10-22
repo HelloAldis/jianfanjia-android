c595abff40e7f2d0a8fb79dc44e8e60e520ada76
feng0413 <041332feng@163.com>
2015-10-22 15:48:54 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/activity/DesignerPlanListActivity.java
+++ app/src/main/java/com/jianfanjia/cn/activity/DesignerPlanListActivity.java
@@ -1,6 +1,14 @@
 package com.jianfanjia.cn.activity;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ListView;
+
 import com.jianfanjia.cn.base.BaseActivity;
+import com.jianfanjia.cn.http.JianFanJiaClient;
+import com.jianfanjia.cn.interf.ApiUiUpdateListener;
+import com.jianfanjia.cn.tools.LogTool;
+import com.jianfanjia.cn.view.MainHeadView;
 
 /**
  * Description:设计师方案列表
@@ -8,17 +16,61 @@ import com.jianfanjia.cn.base.BaseActivity;
  * Email：leo.feng@myjyz.com
  * Date:15-10-11 14:30
  */
-public class DesignerPlanListActivity extends BaseActivity {
-
+public class DesignerPlanListActivity extends BaseActivity implements OnClickListener, ApiUiUpdateListener {
+    private static final String TAG = DesignerPlanListActivity.class.getName();
+    private MainHeadView mainHeadView = null;
+    private ListView designer_plan_listview = null;
 
     @Override
     public void initView() {
+        initMainHeadView();
+        designer_plan_listview = (ListView) findViewById(R.id.designer_plan_listview);
+    }
 
+    private void initMainHeadView() {
+        mainHeadView = (MainHeadView) findViewById(R.id.my_plan_head_layout);
+        mainHeadView.setBackListener(this);
+        mainHeadView
+                .setMianTitle(getResources().getString(R.string.planText));
+        mainHeadView.setLayoutBackground(R.color.head_layout_bg);
+        mainHeadView.setRightTitleVisable(View.GONE);
+        mainHeadView.setBackLayoutVisable(View.VISIBLE);
     }
 
     @Override
     public void setListener() {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.head_back_layout:
+                finish();
+                break;
+            default:
+                break;
+        }
+    }
+
+    //业主获取我的方案
+    private void getDesignerPlansList(String requestmentid, String designerid) {
+        JianFanJiaClient.getDesignerPlansByUser(DesignerPlanListActivity.this, requestmentid, designerid, this, this);
+    }
+
+    @Override
+    public void preLoad() {
+
+    }
+
+    @Override
+    public void loadSuccess(Object data) {
+        LogTool.d(TAG, "data:" + data);
+    }
+
+    @Override
+    public void loadFailture(String error_msg) {
+        makeTextLong(error_msg);
     }
 
     @Override
