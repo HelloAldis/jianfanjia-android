424e07c7f118658712247dfeda27b2f742ccc602
feng0413 <041332feng@163.com>
2015-10-22 09:36:07 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/config/Global.java
+++ app/src/main/java/com/jianfanjia/cn/config/Global.java
@@ -12,4 +12,5 @@ public class Global {
     public static final String REQUIREMENT_ID = "requirement_id";
 
     public static final String DESIGNER_INF0 = "designer_info";
+    public static final String DESIGNER_ID = "designer_id";
 }
