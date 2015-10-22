808ab3951f248366d52dc8d6a089c6a70987af33
feng0413 <041332feng@163.com>
2015-10-22 18:59:27 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/adapter/DesignerPlanAdapter.java
+++ app/src/main/java/com/jianfanjia/cn/adapter/DesignerPlanAdapter.java
@@ -6,6 +6,8 @@ import android.view.View;
 
 import com.jianfanjia.cn.activity.R;
 import com.jianfanjia.cn.adapter.base.BaseListAdapter;
+import com.jianfanjia.cn.interf.ItemClickListener;
+import com.jianfanjia.cn.interf.ViewPagerClickListener;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,13 +19,20 @@ import java.util.List;
  * Time: 17:54
  */
 public class DesignerPlanAdapter extends BaseListAdapter<List<HashMap<String, Object>>> {
+    private ItemClickListener itemClickListener;
 
     public DesignerPlanAdapter(Context context, List<List<HashMap<String, Object>>> list) {
         super(context, list);
     }
 
+    public DesignerPlanAdapter(Context context, List<List<HashMap<String, Object>>> list, ItemClickListener itemClickListener) {
+        super(context, list);
+        this.itemClickListener = itemClickListener;
+    }
+
+
     @Override
-    public View initView(int position, View convertView) {
+    public View initView(final int position, View convertView) {
         ViewHolder holder = null;
         List<HashMap<String, Object>> arrayListForEveryGridView = list.get(position);
         if (convertView == null) {
@@ -36,7 +45,12 @@ public class DesignerPlanAdapter extends BaseListAdapter<List<HashMap<String, Ob
             holder = (ViewHolder) convertView.getTag();
         }
 
-        PlanViewAdapter adapter = new PlanViewAdapter(context, arrayListForEveryGridView);
+        PlanViewAdapter adapter = new PlanViewAdapter(context, arrayListForEveryGridView, new ViewPagerClickListener() {
+            @Override
+            public void onClickItem(int pos) {
+                itemClickListener.onCallBack(position, pos);
+            }
+        });
         holder.vp.setAdapter(adapter);
 
         return convertView;
--- app/src/main/java/com/jianfanjia/cn/adapter/PlanViewAdapter.java
+++ app/src/main/java/com/jianfanjia/cn/adapter/PlanViewAdapter.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.jianfanjia.cn.activity.R;
+import com.jianfanjia.cn.interf.ViewPagerClickListener;
 
 import java.util.HashMap;
 import java.util.List;
@@ -23,10 +24,17 @@ public class PlanViewAdapter extends PagerAdapter {
     private static final String TAG = "PlanViewAdapter";
     private Context context;
     private List<HashMap<String, Object>> mList;
+    private ViewPagerClickListener itemClickListener;
 
     public PlanViewAdapter(Context context, List<HashMap<String, Object>> mList) {
         this.context = context;
         this.mList = mList;
+    }
+
+    public PlanViewAdapter(Context context, List<HashMap<String, Object>> mList, ViewPagerClickListener itemClickListener) {
+        this.context = context;
+        this.mList = mList;
+        this.itemClickListener = itemClickListener;
     }
 
 
@@ -57,15 +65,19 @@ public class PlanViewAdapter extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, final int position) {
         View view = LayoutInflater.from(context).inflate(
-                R.layout.fragment_home_designerinfo_item, container, false);
+                R.layout.list_item_plan_view_item, container, false);
         ImageView imageView = (ImageView) view
-                .findViewById(R.id.list_item_head_img);
-        TextView titleView = (TextView) view
-                .findViewById(R.id.list_item_designer_text);
+                .findViewById(R.id.list_item_plan_img);
         HashMap<String, Object> hashMap = this.mList.get(position);
         imageView.setImageResource((int) hashMap.get("content"));
-        titleView.setText("hello");
         container.addView(view, 0);
+        view.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                itemClickListener.onClickItem(position);
+            }
+        });
         return view;
     }
 
