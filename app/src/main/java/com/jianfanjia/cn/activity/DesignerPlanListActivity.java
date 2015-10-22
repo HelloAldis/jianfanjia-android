1d26c6f663fe93138685e73c4aac5c1a85297bed
feng0413 <041332feng@163.com>
2015-10-22 11:02:40 +0800

    commit  JianFanJiaClient

---
+++ app/src/main/java/com/jianfanjia/cn/activity/DesignerPlanListActivity.java
@@ -0,0 +1,29 @@
+package com.jianfanjia.cn.activity;
+
+import com.jianfanjia.cn.base.BaseActivity;
+
+/**
+ * Description:设计师方案列表
+ * Author：fengliang
+ * Email：leo.feng@myjyz.com
+ * Date:15-10-11 14:30
+ */
+public class DesignerPlanListActivity extends BaseActivity {
+
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_designer_plan_list;
+    }
+
+}
