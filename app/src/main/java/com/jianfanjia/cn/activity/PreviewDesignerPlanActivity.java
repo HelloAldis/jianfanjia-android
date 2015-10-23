bf1ca34c5eeacf810ac7524b07264f2c3d8a1f39
feng0413 <041332feng@163.com>
2015-10-23 09:08:50 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/activity/PreviewDesignerPlanActivity.java
+++ app/src/main/java/com/jianfanjia/cn/activity/PreviewDesignerPlanActivity.java
@@ -2,8 +2,12 @@ package com.jianfanjia.cn.activity;
 
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.jianfanjia.cn.base.BaseActivity;
+import com.jianfanjia.cn.http.JianFanJiaClient;
+import com.jianfanjia.cn.interf.ApiUiUpdateListener;
+import com.jianfanjia.cn.tools.LogTool;
 import com.jianfanjia.cn.view.MainHeadView;
 
 /**
@@ -15,10 +19,12 @@ import com.jianfanjia.cn.view.MainHeadView;
 public class PreviewDesignerPlanActivity extends BaseActivity implements OnClickListener {
     private static final String TAG = PreviewDesignerPlanActivity.class.getName();
     private MainHeadView mainHeadView = null;
+    private Button btnDetail = null;
 
     @Override
     public void initView() {
         initMainHeadView();
+        btnDetail = (Button) findViewById(R.id.btnDetail);
     }
 
     private void initMainHeadView() {
@@ -35,7 +41,7 @@ public class PreviewDesignerPlanActivity extends BaseActivity implements OnClick
 
     @Override
     public void setListener() {
-
+        btnDetail.setOnClickListener(this);
     }
 
     @Override
@@ -45,12 +51,64 @@ public class PreviewDesignerPlanActivity extends BaseActivity implements OnClick
                 finish();
                 break;
             case R.id.head_right_title:
-
+                makeTextLong("详细报价");
+                break;
+            case R.id.btnDetail:
+                makeTextLong("详细报价");
                 break;
             default:
                 break;
         }
     }
+
+    //获取某个方案信息
+    private void getPlanInfo(String planid) {
+        JianFanJiaClient.getPlanInfo(PreviewDesignerPlanActivity.this, planid, getPlanInfoListener, this);
+    }
+
+
+    //选的方案
+    private void chooseDesignerPlan(String requirementid, String designerid, String planid) {
+        JianFanJiaClient.chooseDesignerPlan(PreviewDesignerPlanActivity.this, requirementid, designerid, planid, chooseDesignerPlanListener, this);
+    }
+
+
+    private ApiUiUpdateListener getPlanInfoListener = new ApiUiUpdateListener() {
+        @Override
+        public void preLoad() {
+
+        }
+
+        @Override
+        public void loadSuccess(Object data) {
+            LogTool.d(TAG, "data:" + data);
+
+        }
+
+        @Override
+        public void loadFailture(String error_msg) {
+            makeTextLong(error_msg);
+        }
+    };
+
+
+    private ApiUiUpdateListener chooseDesignerPlanListener = new ApiUiUpdateListener() {
+        @Override
+        public void preLoad() {
+
+        }
+
+        @Override
+        public void loadSuccess(Object data) {
+            LogTool.d(TAG, "data:" + data);
+
+        }
+
+        @Override
+        public void loadFailture(String error_msg) {
+            makeTextLong(error_msg);
+        }
+    };
 
     @Override
     public int getLayoutId() {
