fa3e702fac9bac0f51c286371ee1321092ee0eb6
feng0413 <041332feng@163.com>
2015-10-23 09:42:16 +0800

    commit  JianFanJiaClient

---
+++ app/src/main/java/com/jianfanjia/cn/http/request/GetContractRequest.java
@@ -0,0 +1,39 @@
+package com.jianfanjia.cn.http.request;
+
+import android.content.Context;
+
+import com.jianfanjia.cn.base.BaseRequest;
+import com.jianfanjia.cn.config.Url_New;
+
+/**
+ * Name: GetContractRequest
+ * User: fengliang
+ * Date: 2015-10-23
+ * Time: 09:38
+ */
+public class GetContractRequest extends BaseRequest {
+    private String requirementid;
+
+
+    public GetContractRequest(Context context, String requirementid) {
+        super(context);
+        this.requirementid = requirementid;
+        url = Url_New.ONE_CONTRACT;
+    }
+
+    @Override
+    public void pre() {
+        super.pre();
+    }
+
+    @Override
+    public void all() {
+        super.all();
+    }
+
+    @Override
+    public void onSuccess(Object data) {
+        super.onSuccess(data);
+    }
+}
+
