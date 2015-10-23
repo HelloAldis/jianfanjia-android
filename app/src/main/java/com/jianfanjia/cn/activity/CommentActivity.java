7494fe58fb77a872f123b10bc50b863c248f7e7b
feng0413 <041332feng@163.com>
2015-10-23 18:37:24 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/activity/CommentActivity.java
+++ app/src/main/java/com/jianfanjia/cn/activity/CommentActivity.java
@@ -1,18 +1,50 @@
 package com.jianfanjia.cn.activity;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+
 import com.jianfanjia.cn.base.BaseActivity;
+import com.jianfanjia.cn.view.MainHeadView;
 
-public class CommentActivity extends BaseActivity {
-
+/**
+ * Description:评论留言
+ * Author：fengliang
+ * Email：leo.feng@myjyz.com
+ * Date:15-10-11 14:30
+ */
+public class CommentActivity extends BaseActivity implements OnClickListener {
+    private static final String TAG = CommentActivity.class.getName();
+    private MainHeadView mainHeadView = null;
 
     @Override
     public void initView() {
+        initMainHeadView();
+    }
 
+    private void initMainHeadView() {
+        mainHeadView = (MainHeadView) findViewById(R.id.my_comment_head_layout);
+        mainHeadView.setBackListener(this);
+        mainHeadView
+                .setMianTitle(getResources().getString(R.string.commentText));
+        mainHeadView.setLayoutBackground(R.color.head_layout_bg);
+        mainHeadView.setRightTitleVisable(View.GONE);
+        mainHeadView.setBackLayoutVisable(View.VISIBLE);
     }
 
     @Override
     public void setListener() {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.head_back_layout:
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
