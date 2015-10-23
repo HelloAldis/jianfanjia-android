67ec03c3420f91b4193d8888ddda9caa7ce2a031
feng0413 <041332feng@163.com>
2015-10-23 15:25:20 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/activity/DetailPriceActivity.java
+++ app/src/main/java/com/jianfanjia/cn/activity/DetailPriceActivity.java
@@ -1,6 +1,10 @@
 package com.jianfanjia.cn.activity;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+
 import com.jianfanjia.cn.base.BaseActivity;
+import com.jianfanjia.cn.view.MainHeadView;
 
 /**
  * Description:方案详细报价
@@ -8,17 +12,38 @@ import com.jianfanjia.cn.base.BaseActivity;
  * Email：leo.feng@myjyz.com
  * Date:15-10-11 14:30
  */
-public class DetailPriceActivity extends BaseActivity {
-
+public class DetailPriceActivity extends BaseActivity implements OnClickListener {
+    private static final String TAG = DetailPriceActivity.class.getName();
+    private MainHeadView mainHeadView = null;
 
     @Override
     public void initView() {
+        initMainHeadView();
+    }
 
+    private void initMainHeadView() {
+        mainHeadView = (MainHeadView) findViewById(R.id.my_price_head_layout);
+        mainHeadView.setBackListener(this);
+        mainHeadView
+                .setMianTitle(getResources().getString(R.string.str_view_price));
+        mainHeadView.setLayoutBackground(R.color.head_layout_bg);
+        mainHeadView.setDividerVisable(View.VISIBLE);
     }
 
     @Override
     public void setListener() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.head_back_layout:
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
