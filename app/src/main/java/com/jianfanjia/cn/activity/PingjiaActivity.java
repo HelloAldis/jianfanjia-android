909a52fe36f2b1cfe7d5e3a4fdc98732cc76bf4e
feng0413 <041332feng@163.com>
2015-10-23 10:30:37 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/activity/PingjiaActivity.java
+++ app/src/main/java/com/jianfanjia/cn/activity/PingjiaActivity.java
@@ -1,10 +1,16 @@
 package com.jianfanjia.cn.activity;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.RatingBar.OnRatingBarChangeListener;
 
 import com.jianfanjia.cn.base.BaseActivity;
+import com.jianfanjia.cn.http.JianFanJiaClient;
+import com.jianfanjia.cn.interf.ApiUiUpdateListener;
 import com.jianfanjia.cn.tools.LogTool;
+import com.jianfanjia.cn.view.MainHeadView;
 
 /**
  * Description:评价设计师
@@ -13,24 +19,52 @@ import com.jianfanjia.cn.tools.LogTool;
  * Date:15-10-11 14:30
  */
 public class PingjiaActivity extends BaseActivity implements
-        OnRatingBarChangeListener {
+        OnRatingBarChangeListener, OnClickListener, ApiUiUpdateListener {
     private static final String TAG = PingjiaActivity.class.getName();
-    private RatingBar bar;
-    private RatingBar speedBar;
-    private RatingBar attudeBar;
+    private MainHeadView mainHeadView = null;
+    private RatingBar bar = null;
+    private RatingBar speedBar = null;
+    private RatingBar attudeBar = null;
+    private Button btn_commit = null;
 
 
     @Override
     public void initView() {
+        initMainHeadView();
         bar = (RatingBar) findViewById(R.id.ratingBar);
         speedBar = (RatingBar) findViewById(R.id.speedBar);
         attudeBar = (RatingBar) findViewById(R.id.attudeBar);
+        btn_commit = (Button) findViewById(R.id.btn_commit);
     }
+
+    private void initMainHeadView() {
+        mainHeadView = (MainHeadView) findViewById(R.id.my_pingjia_head_layout);
+        mainHeadView.setBackListener(this);
+        mainHeadView
+                .setMianTitle(getResources().getString(R.string.pingjiaText));
+        mainHeadView.setLayoutBackground(R.color.head_layout_bg);
+        mainHeadView.setRightTitleVisable(View.GONE);
+        mainHeadView.setBackLayoutVisable(View.VISIBLE);
+    }
+
 
     @Override
     public void setListener() {
-        //设置监听器
         speedBar.setOnRatingBarChangeListener(this);
+        btn_commit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.head_back_layout:
+                finish();
+                break;
+            case R.id.btn_commit:
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
@@ -59,6 +93,28 @@ public class PingjiaActivity extends BaseActivity implements
         }
         LogTool.d(TAG, " result:" + result);
         makeTextLong(" result:" + result);
+    }
+
+    //评价设计师
+    private void evaluateDesignerByUser(String requirementid, String designerid, int service_attitude, int respond_speed, String comment, String is_anonymous) {
+        JianFanJiaClient.evaluateDesignerByUser(PingjiaActivity.this, requirementid, designerid, service_attitude, respond_speed, comment, is_anonymous, this, this);
+    }
+
+    @Override
+    public void preLoad() {
+
+    }
+
+    @Override
+    public void loadSuccess(Object data) {
+        super.loadSuccess(data);
+        LogTool.d(TAG, "data:" + data);
+    }
+
+
+    @Override
+    public void loadFailture(String error_msg) {
+        makeTextLong(error_msg);
     }
 
     @Override
