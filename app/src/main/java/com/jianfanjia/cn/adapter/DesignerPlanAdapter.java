6b220b379ee41e92895fe00aa3eb0cf66e4269ff
feng0413 <041332feng@163.com>
2015-10-23 12:27:26 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/adapter/DesignerPlanAdapter.java
+++ app/src/main/java/com/jianfanjia/cn/adapter/DesignerPlanAdapter.java
@@ -6,10 +6,10 @@ import android.view.View;
 
 import com.jianfanjia.cn.activity.R;
 import com.jianfanjia.cn.adapter.base.BaseListAdapter;
+import com.jianfanjia.cn.bean.PlanInfo;
 import com.jianfanjia.cn.interf.ItemClickListener;
 import com.jianfanjia.cn.interf.ViewPagerClickListener;
 
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -18,14 +18,14 @@ import java.util.List;
  * Date: 2015-10-22
  * Time: 17:54
  */
-public class DesignerPlanAdapter extends BaseListAdapter<List<HashMap<String, Object>>> {
+public class DesignerPlanAdapter extends BaseListAdapter<PlanInfo> {
     private ItemClickListener itemClickListener;
 
-    public DesignerPlanAdapter(Context context, List<List<HashMap<String, Object>>> list) {
+    public DesignerPlanAdapter(Context context, List<PlanInfo> list) {
         super(context, list);
     }
 
-    public DesignerPlanAdapter(Context context, List<List<HashMap<String, Object>>> list, ItemClickListener itemClickListener) {
+    public DesignerPlanAdapter(Context context, List<PlanInfo> list, ItemClickListener itemClickListener) {
         super(context, list);
         this.itemClickListener = itemClickListener;
     }
@@ -34,7 +34,7 @@ public class DesignerPlanAdapter extends BaseListAdapter<List<HashMap<String, Ob
     @Override
     public View initView(final int position, View convertView) {
         ViewHolder holder = null;
-        List<HashMap<String, Object>> arrayListForEveryGridView = list.get(position);
+        PlanInfo info = list.get(position);
         if (convertView == null) {
             convertView = layoutInflater.inflate(R.layout.list_item_plan_info,
                     null);
@@ -44,8 +44,8 @@ public class DesignerPlanAdapter extends BaseListAdapter<List<HashMap<String, Ob
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
-
-        PlanViewAdapter adapter = new PlanViewAdapter(context, arrayListForEveryGridView, new ViewPagerClickListener() {
+        List<String> imgList = info.getImages();
+        PlanViewAdapter adapter = new PlanViewAdapter(context, imgList, new ViewPagerClickListener() {
             @Override
             public void onClickItem(int pos) {
                 itemClickListener.onCallBack(position, pos);
