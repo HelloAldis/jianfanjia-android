0fdf5fe6fd417e445d223ffba63c8dcea61e07c1
feng0413 <041332feng@163.com>
2015-10-23 15:14:38 +0800

    commit  JianFanJiaClient

---
+++ app/src/main/java/com/jianfanjia/cn/activity/DetailPriceActivity.java
@@ -0,0 +1,29 @@
+package com.jianfanjia.cn.activity;
+
+import com.jianfanjia.cn.base.BaseActivity;
+
+/**
+ * Description:方案详细报价
+ * Author：fengliang
+ * Email：leo.feng@myjyz.com
+ * Date:15-10-11 14:30
+ */
+public class DetailPriceActivity extends BaseActivity {
+
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_detail_price;
+    }
+
+}
