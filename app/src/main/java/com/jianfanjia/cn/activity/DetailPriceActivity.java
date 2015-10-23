b5c57aa8ba4f5cda53c07e186f06fcd312e52bca
feng0413 <041332feng@163.com>
2015-10-23 17:08:20 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/activity/DetailPriceActivity.java
+++ app/src/main/java/com/jianfanjia/cn/activity/DetailPriceActivity.java
@@ -1,9 +1,17 @@
 package com.jianfanjia.cn.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.jianfanjia.cn.adapter.PriceDetailAdapter;
 import com.jianfanjia.cn.base.BaseActivity;
+import com.jianfanjia.cn.bean.PlandetailInfo;
+import com.jianfanjia.cn.config.Global;
+import com.jianfanjia.cn.tools.LogTool;
 import com.jianfanjia.cn.view.MainHeadView;
 
 /**
@@ -15,19 +23,37 @@ import com.jianfanjia.cn.view.MainHeadView;
 public class DetailPriceActivity extends BaseActivity implements OnClickListener {
     private static final String TAG = DetailPriceActivity.class.getName();
     private MainHeadView mainHeadView = null;
+    private ListView priceListView = null;
+    private TextView totalPrice = null;
+    private PriceDetailAdapter adapter = null;
+    private PlandetailInfo planDetailInfo = null;
 
     @Override
     public void initView() {
         initMainHeadView();
+        priceListView = (ListView) findViewById(R.id.price_listview);
+        totalPrice = (TextView) findViewById(R.id.priceText);
+        Intent intent = this.getIntent();
+        Bundle priceBundle = intent.getExtras();
+        planDetailInfo = (PlandetailInfo) priceBundle.getSerializable(Global.PLAN_DETAIL);
+        LogTool.d(TAG, "planDetailInfo =" + planDetailInfo);
+        if (null != planDetailInfo) {
+            totalPrice.setText("项目总造价:" + planDetailInfo.getTotal_price() + "元");
+            adapter = new PriceDetailAdapter(DetailPriceActivity.this, planDetailInfo.getPrice_detail());
+            priceListView.setAdapter(adapter);
+        }
     }
 
     private void initMainHeadView() {
         mainHeadView = (MainHeadView) findViewById(R.id.my_price_head_layout);
         mainHeadView.setBackListener(this);
+        mainHeadView.setRightTextListener(this);
         mainHeadView
                 .setMianTitle(getResources().getString(R.string.str_view_price));
+        mainHeadView.setRightTitle(getResources().getString(R.string.okText));
         mainHeadView.setLayoutBackground(R.color.head_layout_bg);
-        mainHeadView.setDividerVisable(View.VISIBLE);
+        mainHeadView.setRightTitleVisable(View.VISIBLE);
+        mainHeadView.setBackLayoutVisable(View.VISIBLE);
     }
 
     @Override
@@ -40,6 +66,9 @@ public class DetailPriceActivity extends BaseActivity implements OnClickListener
         switch (v.getId()) {
             case R.id.head_back_layout:
                 finish();
+                break;
+            case R.id.head_right_title:
+                makeTextLong("确定");
                 break;
             default:
                 break;
