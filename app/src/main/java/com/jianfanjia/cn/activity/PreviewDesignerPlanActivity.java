9e43d8a23da7c8b42484c9f06f126e0f64a0b891
feng0413 <041332feng@163.com>
2015-10-23 15:50:06 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/activity/PreviewDesignerPlanActivity.java
+++ app/src/main/java/com/jianfanjia/cn/activity/PreviewDesignerPlanActivity.java
@@ -1,14 +1,23 @@
 package com.jianfanjia.cn.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.jianfanjia.cn.adapter.PlanViewAdapter;
 import com.jianfanjia.cn.base.BaseActivity;
+import com.jianfanjia.cn.bean.PlandetailInfo;
+import com.jianfanjia.cn.config.Global;
 import com.jianfanjia.cn.http.JianFanJiaClient;
 import com.jianfanjia.cn.interf.ApiUiUpdateListener;
+import com.jianfanjia.cn.tools.JsonParser;
 import com.jianfanjia.cn.tools.LogTool;
 import com.jianfanjia.cn.view.MainHeadView;
+
+import java.util.List;
 
 /**
  * Description:预览设计师方案
@@ -19,14 +28,24 @@ import com.jianfanjia.cn.view.MainHeadView;
 public class PreviewDesignerPlanActivity extends BaseActivity implements OnClickListener {
     private static final String TAG = PreviewDesignerPlanActivity.class.getName();
     private MainHeadView mainHeadView = null;
+    private ViewPager viewPager = null;
     private Button btnDetail = null;
     private Button btn_choose = null;
+    private String designerid = null;
+    private String requirementid = null;
+    private String planid = null;
 
     @Override
     public void initView() {
         initMainHeadView();
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
         btnDetail = (Button) findViewById(R.id.btnDetail);
         btn_choose = (Button) findViewById(R.id.btn_choose);
+        Intent intent = this.getIntent();
+        Bundle planBundle = intent.getExtras();
+        planid = planBundle.getString(Global.PLAN_ID);
+        LogTool.d(TAG, "planid=" + planid);
+        getPlanInfo(planid);
     }
 
     private void initMainHeadView() {
@@ -54,13 +73,13 @@ public class PreviewDesignerPlanActivity extends BaseActivity implements OnClick
                 finish();
                 break;
             case R.id.head_right_title:
-                makeTextLong("详细报价");
+                startActivity(DetailPriceActivity.class);
                 break;
             case R.id.btnDetail:
-                makeTextLong("详细报价");
+                startActivity(DetailPriceActivity.class);
                 break;
             case R.id.btn_choose:
-                chooseDesignerPlan("", "", "");
+                chooseDesignerPlan(requirementid, designerid, planid);
                 break;
             default:
                 break;
@@ -82,18 +101,33 @@ public class PreviewDesignerPlanActivity extends BaseActivity implements OnClick
     private ApiUiUpdateListener getPlanInfoListener = new ApiUiUpdateListener() {
         @Override
         public void preLoad() {
-
+            showWaitDialog(R.string.loading);
         }
 
         @Override
         public void loadSuccess(Object data) {
             LogTool.d(TAG, "data:" + data);
-
+            hideWaitDialog();
+            PlandetailInfo planDetailInfo = JsonParser.jsonToBean(data.toString(), PlandetailInfo.class);
+            LogTool.d(TAG, "planDetailInfo:" + planDetailInfo);
+            if (null != planDetailInfo) {
+                requirementid = planDetailInfo.getRequirementid();
+                designerid = planDetailInfo.getDesignerid();
+                LogTool.d(TAG, "requirementid:" + requirementid + " designerid:" + designerid);
+                String planStatus = planDetailInfo.getStatus();
+                if (planStatus.equals(Global.PLAN_STATUS5)) {
+                    btn_choose.setEnabled(false);
+                }
+                List<String> imgList = planDetailInfo.getImages();
+                PlanViewAdapter adapter = new PlanViewAdapter(PreviewDesignerPlanActivity.this, imgList);
+                viewPager.setAdapter(adapter);
+            }
         }
 
         @Override
         public void loadFailture(String error_msg) {
             makeTextLong(error_msg);
+            hideWaitDialog();
         }
     };
 
@@ -101,18 +135,20 @@ public class PreviewDesignerPlanActivity extends BaseActivity implements OnClick
     private ApiUiUpdateListener chooseDesignerPlanListener = new ApiUiUpdateListener() {
         @Override
         public void preLoad() {
-
+            showWaitDialog(R.string.submiting);
         }
 
         @Override
         public void loadSuccess(Object data) {
             LogTool.d(TAG, "data:" + data);
-
+            makeTextLong(data.toString());
+            hideWaitDialog();
         }
 
         @Override
         public void loadFailture(String error_msg) {
             makeTextLong(error_msg);
+            hideWaitDialog();
         }
     };
 
