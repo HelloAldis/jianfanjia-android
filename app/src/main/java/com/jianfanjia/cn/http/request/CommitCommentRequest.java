fa2dcff605287a394dea2762c91515d2bfcc3c0c
feng0413 <041332feng@163.com>
2015-10-23 19:36:48 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/http/request/CommitCommentRequest.java
+++ app/src/main/java/com/jianfanjia/cn/http/request/CommitCommentRequest.java
@@ -6,31 +6,31 @@ import com.jianfanjia.cn.base.BaseRequest;
 import com.jianfanjia.cn.config.Url;
 
 public class CommitCommentRequest extends BaseRequest {
-	
-	public CommitCommentRequest(Context context) {
-		super(context);
-		url = Url.POST_PROCESS_COMMENT;
-	}
 
-	
-	@Override
-	public void all() {
-		// TODO Auto-generated method stub
-		super.all();
-		
-	}
-	
-	@Override
-	public void pre() {
-		// TODO Auto-generated method stub
-		super.pre();
-	}
-	
-	@Override
-	public void onSuccess(Object data) {
-		if(data != null){
-		}
-	}
+    public CommitCommentRequest(Context context) {
+        super(context);
+        url = Url.POST_PROCESS_COMMENT;
+    }
+
+
+    @Override
+    public void all() {
+        // TODO Auto-generated method stub
+        super.all();
+
+    }
+
+    @Override
+    public void pre() {
+        // TODO Auto-generated method stub
+        super.pre();
+    }
+
+    @Override
+    public void onSuccess(Object data) {
+        if (data != null) {
+        }
+    }
 
 
 }
