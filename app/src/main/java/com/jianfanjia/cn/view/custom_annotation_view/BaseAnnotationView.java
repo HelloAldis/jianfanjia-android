88b923f80342a82af803c5272056aa893f2f8841
zhhao226 <352233265@qq.com>
2015-10-26 11:20:25 +0800

    commit some problem

---
+++ app/src/main/java/com/jianfanjia/cn/view/custom_annotation_view/BaseAnnotationView.java
@@ -0,0 +1,10 @@
+package com.jianfanjia.cn.view.custom_annotation_view;
+
+/**
+ * Description: com.jianfanjia.cn.view.custom_annotation_view
+ * Author: zhanghao
+ * Email: jame.zhang@myjyz.com
+ * Date:2015-10-26 10:50
+ */
+public class BaseAnnotationView {
+}
