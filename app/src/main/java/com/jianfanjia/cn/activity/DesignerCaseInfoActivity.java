a436dc54743f3968f0806093c56ccf56a0b29ba9
feng0413 <041332feng@163.com>
2015-10-28 15:23:42 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/activity/DesignerCaseInfoActivity.java
+++ app/src/main/java/com/jianfanjia/cn/activity/DesignerCaseInfoActivity.java
@@ -125,7 +125,7 @@ public class DesignerCaseInfoActivity extends BaseActivity implements ApiUiUpdat
         if (null != designerCaseInfo) {
             designertid = designerCaseInfo.getDesigner().get_id();
             collapsingToolbar.setTitle(designerCaseInfo.getCell());
-            stylelText.setText(designerCaseInfo.getHouse_area() + "㎡");
+            stylelText.setText(designerCaseInfo.getHouse_area() + "㎡," + getHouseType(designerCaseInfo.getHouse_type()) + "," + getDecStyle(designerCaseInfo.getDec_type()));
             imageLoader.displayImage(Url_New.GET_IMAGE + designerCaseInfo.getDesigner().getImageid(), designerinfo_head_img, options);
             produceText.setText("设计简介:" + designerCaseInfo.getDescription());
             adapter = new DesignerCaseAdapter(DesignerCaseInfoActivity.this, designerCaseInfo.getImages());
