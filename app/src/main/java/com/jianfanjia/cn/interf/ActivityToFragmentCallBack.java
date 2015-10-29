ff94b40aecf9f51a7b55249094d641f34b44de08
feng0413 <041332feng@163.com>
2015-10-29 16:20:04 +0800

    commit  JianFanJiaClient

---
+++ app/src/main/java/com/jianfanjia/cn/interf/ActivityToFragmentCallBack.java
@@ -0,0 +1,11 @@
+package com.jianfanjia.cn.interf;
+
+/**
+ * Name: ActivityToFragmentCallBack
+ * User: fengliang
+ * Date: 2015-10-29
+ * Time: 16:17
+ */
+public interface ActivityToFragmentCallBack {
+    void onTransmit(String params);
+}  
