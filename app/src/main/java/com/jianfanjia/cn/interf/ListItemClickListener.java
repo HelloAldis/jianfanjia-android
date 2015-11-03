66ef355e0a082e0638b80ed7a801f882f08bc53b
feng0413 <041332feng@163.com>
2015-11-03 14:32:27 +0800

    commit  JianFanJiaClient

--- app/src/main/java/com/jianfanjia/cn/interf/ListItemClickListener.java
+++ app/src/main/java/com/jianfanjia/cn/interf/ListItemClickListener.java
@@ -4,6 +4,11 @@ package com.jianfanjia.cn.interf;
  * Created by Administrator on 2015/10/15.
  */
 public interface ListItemClickListener {
+
+    void onBtnClick(int position);
+
+    void onGirdItemClick(int position);
+
     void onMaxClick(int position);
 
     void onMinClick(int position);
