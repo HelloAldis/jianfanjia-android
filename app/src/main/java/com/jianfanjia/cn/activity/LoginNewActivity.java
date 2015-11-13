123ddf630be4eb407c87f98faf222293dfa07375
zhhao226 <352233265@qq.com>
2015-11-13 14:30:54 +0800

    update code

--- app/src/main/java/com/jianfanjia/cn/activity/LoginNewActivity.java
+++ app/src/main/java/com/jianfanjia/cn/activity/LoginNewActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
@@ -11,6 +12,7 @@ import android.util.TypedValue;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
@@ -113,6 +115,36 @@ public class LoginNewActivity extends BaseAnnotationActivity implements
             }, 300);
         }
 
+    }
+
+    private void controlKeyboardLayout(final View root,final View scrollToView){
+        root.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        Rect rect = new Rect();
+                        // 获取root在窗体的可视区域
+                        root.getWindowVisibleDisplayFrame(rect);
+                        // 获取root在窗体的不可视区域高度(被其他View遮挡的区域高度)
+                        int rootInvisibleHeight = root.getRootView()
+                                .getHeight() - rect.bottom;
+                        // 若不可视区域高度大于100，则键盘显示
+                        if (rootInvisibleHeight > 100) {
+                            int[] location = new int[2];
+                            // 获取scrollToView在窗体的坐标
+                            scrollToView.getLocationInWindow(location);
+                            // 计算root滚动高度，使scrollToView在可见区域
+                            int srollHeight = (location[1] + scrollToView
+                                    .getHeight()) - rect.bottom;
+//                            root.scrollTo(0, srollHeight);
+                            root.animate().translationY(-srollHeight).setDuration(100).start();
+                        } else {
+                            // 键盘隐藏
+                            root.animate().translationY(0).setDuration(100).start();
+//                            root.scrollTo(0, 0);
+                        }
+                    }
+                });
     }
 
     @Override
