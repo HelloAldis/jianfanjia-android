1535109d028d1513efdd756f420619868d5dfcd1
feng0413 <041332feng@163.com>
2015-11-13 09:29:28 +0800

    commit db

--- app/src/main/java/com.jianfanjia.cn/fragment/YanQiNotifyFragment.java
+++ app/src/main/java/com.jianfanjia.cn/fragment/YanQiNotifyFragment.java
@@ -87,75 +87,6 @@ public class YanQiNotifyFragment extends BaseFragment implements
                 hideWaitDialog();
             }
         }, this);
-        /*JianFanJiaApiClient.rescheduleAll(getActivity(),
-                new JsonHttpResponseHandler() {
-					@Override
-					public void onStart() {
-						LogTool.d(TAG, "onStart()");
-						showWaitDialog();
-					}
-
-					@Override
-					public void onSuccess(int statusCode, Header[] headers,
-							JSONObject response) {
-						LogTool.d(TAG, "JSONObject response:" + response);
-						hideWaitDialog();
-						try {
-							if (response.has(Constant.DATA)) {
-								delayList = JsonParser.jsonToList(
-										response.get(Constant.DATA).toString(),
-										new TypeToken<List<NotifyDelayInfo>>() {
-										}.getType());
-								LogTool.d(TAG, "delayList:" + delayList);
-								delayAdapter = new DelayNotifyAdapter(
-										getActivity(), delayList,
-										new DelayInfoListener() {
-
-											@Override
-											public void onAgree() {
-												if (null != processInfo) {
-													agreeReschedule(processInfo
-															.get_id());
-												}
-											}
-
-											@Override
-											public void onRefuse() {
-												if (null != processInfo) {
-													refuseReschedule(processInfo
-															.get_id());
-												}
-											}
-
-										});
-								yanqiListView.setAdapter(delayAdapter);
-							} else if (response.has(Constant.ERROR_MSG)) {
-								makeTextLong(response.get(Constant.ERROR_MSG)
-										.toString());
-							}
-						} catch (JSONException e) {
-							e.printStackTrace();
-							makeTextLong(getString(R.string.tip_login_error_for_network));
-						}
-					}
-
-					@Override
-					public void onFailure(int statusCode, Header[] headers,
-							Throwable throwable, JSONObject errorResponse) {
-						LogTool.d(TAG,
-								"Throwable throwable:" + throwable.toString());
-						hideWaitDialog();
-						makeTextLong(getString(R.string.tip_login_error_for_network));
-					}
-
-					@Override
-					public void onFailure(int statusCode, Header[] headers,
-							String responseString, Throwable throwable) {
-						LogTool.d(TAG, "throwable:" + throwable);
-						hideWaitDialog();
-						makeTextLong(getString(R.string.tip_login_error_for_network));
-					};
-				});*/
     }
 
     // 用户同意改期
@@ -176,51 +107,6 @@ public class YanQiNotifyFragment extends BaseFragment implements
 
             }
         }, this);
-        /*JianFanJiaApiClient.agreeReschedule(getActivity(), processid,
-                new JsonHttpResponseHandler() {
-					@Override
-					public void onStart() {
-						LogTool.d(this.getClass().getName(), "onStart()");
-					}
-
-					@Override
-					public void onSuccess(int statusCode, Header[] headers,
-							JSONObject response) {
-						LogTool.d(this.getClass().getName(),
-								"JSONObject response:" + response);
-						try {
-							if (response.has(Constant.DATA)) {
-								makeTextLong(response.get(Constant.DATA)
-										.toString());
-							} else if (response.has(Constant.SUCCESS_MSG)) {
-								makeTextLong(response.get(Constant.SUCCESS_MSG)
-										.toString());
-								getRescheduleAll();
-							} else if (response.has(Constant.ERROR_MSG)) {
-								makeTextLong(response.get(Constant.ERROR_MSG)
-										.toString());
-							}
-						} catch (JSONException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-							makeTextLong(getString(R.string.tip_login_error_for_network));
-						}
-					}
-
-					@Override
-					public void onFailure(int statusCode, Header[] headers,
-							Throwable throwable, JSONObject errorResponse) {
-						LogTool.d(this.getClass().getName(),
-								"Throwable throwable:" + throwable.toString());
-					}
-
-					@Override
-					public void onFailure(int statusCode, Header[] headers,
-							String responseString, Throwable throwable) {
-						LogTool.d(this.getClass().getName(), "throwable:"
-								+ throwable);
-					};
-				});*/
     }
 
     // 用户拒绝改期
@@ -241,51 +127,6 @@ public class YanQiNotifyFragment extends BaseFragment implements
 
             }
         }, this);
-        /*JianFanJiaApiClient.refuseReschedule(getActivity(), processid,
-                new JsonHttpResponseHandler() {
-					@Override
-					public void onStart() {
-						LogTool.d(this.getClass().getName(), "onStart()");
-					}
-
-					@Override
-					public void onSuccess(int statusCode, Header[] headers,
-							JSONObject response) {
-						LogTool.d(this.getClass().getName(),
-								"JSONObject response:" + response);
-						try {
-							if (response.has(Constant.DATA)) {
-								makeTextLong(response.get(Constant.DATA)
-										.toString());
-							} else if (response.has(Constant.SUCCESS_MSG)) {
-								makeTextLong(response.get(Constant.SUCCESS_MSG)
-										.toString());
-								getRescheduleAll();
-							} else if (response.has(Constant.ERROR_MSG)) {
-								makeTextLong(response.get(Constant.ERROR_MSG)
-										.toString());
-							}
-						} catch (JSONException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-							makeTextLong(getString(R.string.tip_login_error_for_network));
-						}
-					}
-
-					@Override
-					public void onFailure(int statusCode, Header[] headers,
-							Throwable throwable, JSONObject errorResponse) {
-						LogTool.d(this.getClass().getName(),
-								"Throwable throwable:" + throwable.toString());
-					}
-
-					@Override
-					public void onFailure(int statusCode, Header[] headers,
-							String responseString, Throwable throwable) {
-						LogTool.d(this.getClass().getName(), "throwable:"
-								+ throwable);
-					};
-				});*/
     }
 
     @Override
