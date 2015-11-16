a30158135f943ed42e157d4565b2b96721ed7b65
feng0413 <041332feng@163.com>
2015-11-16 18:44:36 +0800

    commit MainActivity

---
+++ app/src/main/java/com/jianfanjia/cn/Event/UpdateEvent.java
@@ -0,0 +1,19 @@
+package com.jianfanjia.cn.Event;
+
+/**
+ * Name: UpdateEvent
+ * User: fengliang
+ * Date: 2015-11-16
+ * Time: 18:43
+ */
+public class UpdateEvent {
+    private int eventType;
+
+    public int getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(int eventType) {
+        this.eventType = eventType;
+    }
+}
