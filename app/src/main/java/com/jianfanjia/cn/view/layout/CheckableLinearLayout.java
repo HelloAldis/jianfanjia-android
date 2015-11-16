72ae8ca9e2b8e1bb764259b07b08f866695f668b
feng0413 <041332feng@163.com>
2015-11-16 14:20:37 +0800

    commit MainActivity

--- app/src/main/java/com/jianfanjia/cn/view/layout/CheckableLinearLayout.java
+++ app/src/main/java/com/jianfanjia/cn/view/layout/CheckableLinearLayout.java
@@ -1,6 +1,7 @@
 package com.jianfanjia.cn.view.layout;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,10 @@ import android.widget.TextView;
 
 import com.jianfanjia.cn.activity.R;
 import com.jianfanjia.cn.config.Constant;
+import com.jianfanjia.cn.config.Url_New;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 
 /**
  * Name: CheckableLinearLayout
@@ -21,30 +26,47 @@ import com.jianfanjia.cn.config.Constant;
  * Time: 11:20
  */
 public class CheckableLinearLayout extends LinearLayout implements Checkable {
+    private ImageLoader imageLoader;
+    private DisplayImageOptions options;
     private View view = null;
-    private ImageView itemwHeadView;
+    private ImageView itemwHeadView = null;
     private ImageView itemAuthView = null;
-    private TextView itemNameText;
-    private RatingBar itemRatingBar;
-    private CheckBox itemCheck;
+    private TextView itemNameText = null;
+    private TextView itemMarchText = null;
+    private RatingBar itemRatingBar = null;
+    private CheckBox itemCheck = null;
     private int itemType = -1;
 
     public CheckableLinearLayout(Context context, AttributeSet attrs, int itemType) {
         super(context, attrs);
         this.itemType = itemType;
         LayoutInflater inflater = LayoutInflater.from(context);
-        View view = null;
+        imageLoader = ImageLoader.getInstance();
+        options = new DisplayImageOptions.Builder()
+                .showImageOnLoading(R.mipmap.pix_default)
+                .showImageForEmptyUri(R.mipmap.pix_default)
+                .showImageOnFail(R.mipmap.pix_default).cacheInMemory(false)
+                .cacheOnDisk(true).considerExifParams(true)
+                .bitmapConfig(Bitmap.Config.RGB_565).imageScaleType(ImageScaleType.IN_SAMPLE_INT).build();
         if (itemType == Constant.LIST_ITEM_TAG) {
             view = inflater.inflate(R.layout.list_item_designer_by_appoint_tag, this, true);
         } else {
             view = inflater.inflate(R.layout.list_item_designer_by_appoint_info, this, true);
         }
+        itemwHeadView = (ImageView) view.findViewById(R.id.list_item_head_img);
         itemNameText = (TextView) view.findViewById(R.id.list_item_name_text);
+        itemMarchText = (TextView) view.findViewById(R.id.list_item_march_text);
+        itemRatingBar = (RatingBar) view.findViewById(R.id.list_item_ratingBar);
         itemCheck = (CheckBox) view.findViewById(R.id.list_item_check);
     }
 
     public void setName(String text) {
         itemNameText.setText(text);
+    }
+
+
+    public void setHeadView(String imgId) {
+        imageLoader.displayImage(Url_New.GET_THUMBNAIL_IMAGE + imgId, itemwHeadView, options);
     }
 
     @Override
