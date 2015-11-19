57104b7bcd9d4db47fe6b650518cc948007cc3e7
feng0413 <041332feng@163.com>
2015-11-19 17:51:55 +0800

    commit MainActivity

--- app/src/main/java/com/jianfanjia/cn/view/library/PullToRefreshListView.java
+++ app/src/main/java/com/jianfanjia/cn/view/library/PullToRefreshListView.java
@@ -205,7 +205,6 @@ public class PullToRefreshListView extends PullToRefreshAdapterViewBase<ListView
 	@Override
 	protected ListView createRefreshableView(Context context, AttributeSet attrs) {
 		ListView lv = createListView(context, attrs);
-
 		// Set it to this so it can be used in ListActivity/ListFragment
 		lv.setId(android.R.id.list);
 		return lv;
