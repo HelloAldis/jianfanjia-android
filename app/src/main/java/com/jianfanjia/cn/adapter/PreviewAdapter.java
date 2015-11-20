a0935ae15a11dc7094e8589476219d227ac766cc
feng0413 <041332feng@163.com>
2015-11-20 09:32:22 +0800

    commit MainActivity

--- app/src/main/java/com/jianfanjia/cn/adapter/PreviewAdapter.java
+++ app/src/main/java/com/jianfanjia/cn/adapter/PreviewAdapter.java
@@ -1,7 +1,6 @@
 package com.jianfanjia.cn.adapter;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,9 +10,7 @@ import android.widget.ImageView;
 import com.jianfanjia.cn.activity.R;
 import com.jianfanjia.cn.config.Url_New;
 import com.jianfanjia.cn.interf.ViewPagerClickListener;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.jianfanjia.cn.tools.ImageShow;
 
 import java.util.List;
 
@@ -28,21 +25,13 @@ public class PreviewAdapter extends PagerAdapter {
     private ViewPagerClickListener listener;
     private Context context;
     private List<String> mList;
-    private ImageLoader imageLoader;
-    private DisplayImageOptions options;
-
+    private ImageShow imageShow;
 
     public PreviewAdapter(Context context, List<String> mList, ViewPagerClickListener listener) {
         this.context = context;
         this.mList = mList;
         this.listener = listener;
-        imageLoader = ImageLoader.getInstance();
-        options = new DisplayImageOptions.Builder()
-                .showImageOnLoading(R.mipmap.pix_default)
-                .showImageForEmptyUri(R.mipmap.pix_default)
-                .showImageOnFail(R.mipmap.pix_default).cacheInMemory(true)
-                .cacheOnDisk(true).considerExifParams(true)
-                .bitmapConfig(Bitmap.Config.RGB_565).imageScaleType(ImageScaleType.IN_SAMPLE_INT).build();
+        imageShow = ImageShow.getImageShow();
     }
 
 
@@ -77,7 +66,7 @@ public class PreviewAdapter extends PagerAdapter {
         ImageView imageView = (ImageView) view
                 .findViewById(R.id.list_item_plan_img);
         String imgid = mList.get(position);
-        imageLoader.displayImage(Url_New.GET_THUMBNAIL_IMAGE + imgid, imageView, options);
+        imageShow.displayScreenWidthThumnailImage(context, Url_New.GET_THUMBNAIL_IMAGE + imgid, imageView);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
