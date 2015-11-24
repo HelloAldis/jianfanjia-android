fad3941d6c2298843288757239e1515d2687c370
feng0413 <041332feng@163.com>
2015-11-24 16:54:00 +0800

    commit BaseRecyclerViewAdapter

--- app/src/main/java/com/jianfanjia/cn/fragment/DesignerWorksFragment.java
+++ app/src/main/java/com/jianfanjia/cn/fragment/DesignerWorksFragment.java
@@ -1,9 +1,11 @@
 package com.jianfanjia.cn.fragment;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.View;
 
 import com.jianfanjia.cn.activity.DesignerCaseInfoActivity;
@@ -18,6 +20,7 @@ import com.jianfanjia.cn.interf.ApiUiUpdateListener;
 import com.jianfanjia.cn.interf.OnItemClickListener;
 import com.jianfanjia.cn.tools.JsonParser;
 import com.jianfanjia.cn.tools.LogTool;
+import com.jianfanjia.cn.view.baseview.HorizontalDividerItemDecoration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,6 +58,14 @@ public class DesignerWorksFragment extends BaseFragment implements OnItemClickLi
         designer_works_listview.setLayoutManager(mLayoutManager);
         designer_works_listview.setItemAnimator(new DefaultItemAnimator());
         designer_works_listview.setHasFixedSize(true);
+        Paint paint = new Paint();
+        paint.setStrokeWidth(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics()));
+        paint.setAlpha(0);
+        paint.setAntiAlias(true);
+        designer_works_listview.addItemDecoration(new HorizontalDividerItemDecoration.Builder(getActivity())
+                .paint(paint)
+                .showLastDivider()
+                .build());
         designer_works_listview.setFocusable(false);
         getDesignerProduct(designerid, 0, 10);
     }
