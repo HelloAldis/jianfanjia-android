a64596e9e17413fa6498202dd12105300d548a68
feng0413 <041332feng@163.com>
2015-11-30 11:10:01 +0800

    commit BaseRecyclerViewAdapter

--- app/src/main/java/com/jianfanjia/cn/interf/DelayInfoListener.java
+++ app/src/main/java/com/jianfanjia/cn/interf/DelayInfoListener.java
@@ -1,5 +1,5 @@
 package com.jianfanjia.cn.interf;
 
 public interface DelayInfoListener {
-    void onClick(int position, String status);
+    void onClick(int position, String status, String role);
 }
