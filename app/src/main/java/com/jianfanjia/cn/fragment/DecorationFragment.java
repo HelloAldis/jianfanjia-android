6ebfc29f1427f5be46350c47f67c1956aeeb0fa8
feng0413 <041332feng@163.com>
2015-12-08 18:24:16 +0800

    commit URL

--- app/src/main/java/com/jianfanjia/cn/fragment/DecorationFragment.java
+++ app/src/main/java/com/jianfanjia/cn/fragment/DecorationFragment.java
@@ -5,8 +5,18 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 
 import com.jianfanjia.cn.activity.R;
+import com.jianfanjia.cn.adapter.DecorationAdapter;
 import com.jianfanjia.cn.base.BaseFragment;
+import com.jianfanjia.cn.bean.BeautyImgInfo;
+import com.jianfanjia.cn.bean.DecorationItemInfo;
+import com.jianfanjia.cn.http.JianFanJiaClient;
+import com.jianfanjia.cn.interf.ApiUiUpdateListener;
+import com.jianfanjia.cn.interf.OnItemClickListener;
+import com.jianfanjia.cn.tools.JsonParser;
+import com.jianfanjia.cn.tools.LogTool;
 import com.jianfanjia.cn.view.MainHeadView;
+
+import java.util.List;
 
 /**
  * Description:装修美图
@@ -14,17 +24,20 @@ import com.jianfanjia.cn.view.MainHeadView;
  * Email：leo.feng@myjyz.com
  * Date:15-10-11 14:30
  */
-public class DecorationFragment extends BaseFragment {
+public class DecorationFragment extends BaseFragment implements ApiUiUpdateListener {
     private static final String TAG = DecorationFragment.class.getName();
     private MainHeadView mainHeadView = null;
     private RecyclerView decoration_listview = null;
     private StaggeredGridLayoutManager mLayoutManager = null;
+    private DecorationAdapter decorationAdapter = null;
 
     @Override
     public void initView(View view) {
         initMainHeadView(view);
         decoration_listview = (RecyclerView) view.findViewById(R.id.decoration_listview);
         mLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        decoration_listview.setLayoutManager(mLayoutManager);
+        searchDecorationImg();
     }
 
     private void initMainHeadView(View view) {
@@ -40,8 +53,35 @@ public class DecorationFragment extends BaseFragment {
 
     }
 
+    private void searchDecorationImg() {
+        JianFanJiaClient.searchDecorationImg(getContext(), "", -1, "", 0, 5, this, this);
+    }
 
-    private void getDecorationImg() {
+    @Override
+    public void preLoad() {
+
+    }
+
+    @Override
+    public void loadSuccess(Object data) {
+        LogTool.d(TAG, "data:" + data.toString());
+        DecorationItemInfo decorationItemInfo = JsonParser.jsonToBean(data.toString(), DecorationItemInfo.class);
+        LogTool.d(TAG, "decorationItemInfo:" + decorationItemInfo);
+        if (null != decorationItemInfo) {
+            List<BeautyImgInfo> beautyImgList = decorationItemInfo.getBeautiful_images();
+            LogTool.d(TAG, "beautyImgList:" + beautyImgList);
+            decorationAdapter = new DecorationAdapter(getActivity(), beautyImgList, new OnItemClickListener() {
+                @Override
+                public void OnItemClick(int position) {
+
+                }
+            });
+            decoration_listview.setAdapter(decorationAdapter);
+        }
+    }
+
+    @Override
+    public void loadFailture(String error_msg) {
 
     }
 
