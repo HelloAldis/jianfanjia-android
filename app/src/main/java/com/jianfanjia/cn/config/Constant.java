d6d0c3dd0c240e4e2ca0f06acc88f418f620c3f4
feng0413 <041332feng@163.com>
2015-12-10 11:37:40 +0800

    commit URL

--- app/src/main/java/com/jianfanjia/cn/config/Constant.java
+++ app/src/main/java/com/jianfanjia/cn/config/Constant.java
@@ -29,6 +29,7 @@ public class Constant {
     public static final String TEMP_IMG = IMAG_PATH + "/current_camera_temp_path";//拍照的临时存储文件路径
 
     public static final String EXTRA_BUNDLE = "launchBundle";
+    public static final String KEY_WORD = "不限";
 
     public static final int HOME = 0;//首页
     public static final int DECORATE = 1;//装修美图
