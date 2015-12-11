21fd605fe4b0ba30a425ef0f32e9c74be6e88ee5
feng0413 <041332feng@163.com>
2015-12-11 13:25:27 +0800

    commit URL

--- app/src/main/java/com/jianfanjia/cn/bean/ProductInfo.java
+++ app/src/main/java/com/jianfanjia/cn/bean/ProductInfo.java
@@ -11,6 +11,7 @@ import java.util.List;
  */
 public class ProductInfo implements Serializable {
     private List<Product> products;
+
     private int total;
 
     public List<Product> getProducts() {
