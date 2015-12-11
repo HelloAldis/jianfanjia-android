931c66e95d121ace076a4630f9f806144f0c469f
feng0413 <041332feng@163.com>
2015-12-11 18:15:43 +0800

    commit build

--- app/src/main/java/com/jianfanjia/cn/fragment/MyFavoriteDesignerFragment.java
+++ app/src/main/java/com/jianfanjia/cn/fragment/MyFavoriteDesignerFragment.java
@@ -1,12 +1,17 @@
 package com.jianfanjia.cn.fragment;
 
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.jianfanjia.cn.activity.R;
+import com.jianfanjia.cn.adapter.FavoriteDesignerAdapter;
 import com.jianfanjia.cn.base.BaseFragment;
+import com.jianfanjia.cn.bean.MyFavoriteDesigner;
 import com.jianfanjia.cn.http.JianFanJiaClient;
 import com.jianfanjia.cn.interf.ApiUiUpdateListener;
+import com.jianfanjia.cn.tools.JsonParser;
 import com.jianfanjia.cn.tools.LogTool;
 
 /**
@@ -22,6 +27,8 @@ public class MyFavoriteDesignerFragment extends BaseFragment implements ApiUiUpd
     @Override
     public void initView(View view) {
         my_favorite_designer_listview = (RecyclerView) view.findViewById(R.id.my_favorite_designer_listview);
+        my_favorite_designer_listview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        my_favorite_designer_listview.setItemAnimator(new DefaultItemAnimator());
         JianFanJiaClient.get_MyFavoriteDesignerList(getActivity(), 0, 100, this, this);
     }
 
@@ -38,6 +45,12 @@ public class MyFavoriteDesignerFragment extends BaseFragment implements ApiUiUpd
     @Override
     public void loadSuccess(Object data) {
         LogTool.d(TAG, "data=" + data.toString());
+        MyFavoriteDesigner myFavoriteDesigner = JsonParser.jsonToBean(data.toString(), MyFavoriteDesigner.class);
+        LogTool.d(TAG, "myFavoriteDesigner=" + myFavoriteDesigner);
+        if (myFavoriteDesigner != null) {
+            FavoriteDesignerAdapter adapter = new FavoriteDesignerAdapter(getActivity(), myFavoriteDesigner.getDesigners());
+            my_favorite_designer_listview.setAdapter(adapter);
+        }
     }
 
     @Override
