c449bc19bf5e0024c132d8d3d66a0839e2cd77c6
feng0413 <041332feng@163.com>
2015-12-14 17:48:25 +0800

    commit build

--- app/src/main/java/com/jianfanjia/cn/adapter/ProductAdapter.java
+++ app/src/main/java/com/jianfanjia/cn/adapter/ProductAdapter.java
@@ -11,6 +11,7 @@ import com.jianfanjia.cn.adapter.base.BaseRecyclerViewAdapter;
 import com.jianfanjia.cn.adapter.base.RecyclerViewHolderBase;
 import com.jianfanjia.cn.bean.Product;
 import com.jianfanjia.cn.cache.BusinessManager;
+import com.jianfanjia.cn.interf.RecyclerViewOnItemClickListener;
 
 import java.util.List;
 
@@ -21,13 +22,15 @@ import java.util.List;
  * Time: 13:20
  */
 public class ProductAdapter extends BaseRecyclerViewAdapter<Product> {
+    private RecyclerViewOnItemClickListener listener;
 
-    public ProductAdapter(Context context, List<Product> list) {
+    public ProductAdapter(Context context, List<Product> list, RecyclerViewOnItemClickListener listener) {
         super(context, list);
+        this.listener = listener;
     }
 
     @Override
-    public void bindView(RecyclerViewHolderBase viewHolder, int position, List<Product> list) {
+    public void bindView(RecyclerViewHolderBase viewHolder, final int position, List<Product> list) {
         Product product = list.get(position);
         ProductViewHolder holder = (ProductViewHolder) viewHolder;
         holder.itemXiaoQuText.setText(product.getCell());
@@ -36,6 +39,32 @@ public class ProductAdapter extends BaseRecyclerViewAdapter<Product> {
         holder.itemProduceText.setText(product.getHouse_area() + "㎡，" + BusinessManager.convertHouseTypeToShow(houseType) + "，" + BusinessManager.convertDecStyleToShow(decStyle));
         imageShow.displayScreenWidthThumnailImage(context, product.getImages().get(0).getImageid(), holder.itemProductView);
         imageShow.displayImageHeadWidthThumnailImage(context, product.getDesigner().getImageid(), holder.itemHeadView);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != listener) {
+                    listener.OnItemClick(v, position);
+                }
+            }
+        });
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (null != listener) {
+                    listener.OnLongItemClick(v, position);
+                }
+                return true;
+            }
+        });
+        holder.itemHeadView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != listener) {
+                    listener.OnViewClick(position);
+                }
+            }
+        });
+
     }
 
     @Override
