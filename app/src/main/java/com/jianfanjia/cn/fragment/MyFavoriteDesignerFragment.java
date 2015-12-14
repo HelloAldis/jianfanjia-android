67b151b96acfb6d6b9e7490c42dd87438654b198
feng0413 <041332feng@163.com>
2015-12-14 09:53:19 +0800

    commit build

--- app/src/main/java/com/jianfanjia/cn/fragment/MyFavoriteDesignerFragment.java
+++ app/src/main/java/com/jianfanjia/cn/fragment/MyFavoriteDesignerFragment.java
@@ -1,8 +1,10 @@
 package com.jianfanjia.cn.fragment;
 
+import android.graphics.Paint;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.View;
 
 import com.jianfanjia.cn.activity.R;
@@ -13,6 +15,7 @@ import com.jianfanjia.cn.http.JianFanJiaClient;
 import com.jianfanjia.cn.interf.ApiUiUpdateListener;
 import com.jianfanjia.cn.tools.JsonParser;
 import com.jianfanjia.cn.tools.LogTool;
+import com.jianfanjia.cn.view.baseview.HorizontalDividerItemDecoration;
 
 /**
  * @author fengliang
@@ -29,6 +32,11 @@ public class MyFavoriteDesignerFragment extends BaseFragment implements ApiUiUpd
         my_favorite_designer_listview = (RecyclerView) view.findViewById(R.id.my_favorite_designer_listview);
         my_favorite_designer_listview.setLayoutManager(new LinearLayoutManager(getActivity()));
         my_favorite_designer_listview.setItemAnimator(new DefaultItemAnimator());
+        Paint paint = new Paint();
+        paint.setStrokeWidth(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics()));
+        paint.setAlpha(0);
+        paint.setAntiAlias(true);
+        my_favorite_designer_listview.addItemDecoration(new HorizontalDividerItemDecoration.Builder(getActivity()).paint(paint).showLastDivider().build());
         JianFanJiaClient.get_MyFavoriteDesignerList(getActivity(), 0, 100, this, this);
     }
 
