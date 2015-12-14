ca954365924fda850159dc139b6e93a960fdc877
feng0413 <041332feng@163.com>
2015-12-14 10:03:29 +0800

    commit build

--- app/src/main/java/com/jianfanjia/cn/adapter/ProductAdapter.java
+++ app/src/main/java/com/jianfanjia/cn/adapter/ProductAdapter.java
@@ -6,7 +6,7 @@ import android.view.ViewGroup;
 
 import com.jianfanjia.cn.adapter.base.BaseRecyclerViewAdapter;
 import com.jianfanjia.cn.adapter.base.RecyclerViewHolderBase;
-import com.jianfanjia.cn.bean.ProductInfo;
+import com.jianfanjia.cn.bean.Product;
 
 import java.util.List;
 
@@ -16,14 +16,14 @@ import java.util.List;
  * Date: 2015-12-11
  * Time: 13:20
  */
-public class ProductAdapter extends BaseRecyclerViewAdapter<ProductInfo> {
+public class ProductAdapter extends BaseRecyclerViewAdapter<Product> {
 
-    public ProductAdapter(Context context, List<ProductInfo> list) {
+    public ProductAdapter(Context context, List<Product> list) {
         super(context, list);
     }
 
     @Override
-    public void bindView(RecyclerViewHolderBase viewHolder, int position, List<ProductInfo> list) {
+    public void bindView(RecyclerViewHolderBase viewHolder, int position, List<Product> list) {
 
     }
 
--- app/src/main/java/com/jianfanjia/cn/fragment/ProductFragment.java
+++ app/src/main/java/com/jianfanjia/cn/fragment/ProductFragment.java
@@ -1,10 +1,21 @@
 package com.jianfanjia.cn.fragment;
 
+import android.graphics.Paint;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.View;
 
 import com.jianfanjia.cn.activity.R;
+import com.jianfanjia.cn.adapter.ProductAdapter;
 import com.jianfanjia.cn.base.BaseFragment;
+import com.jianfanjia.cn.bean.ProductInfo;
+import com.jianfanjia.cn.http.JianFanJiaClient;
+import com.jianfanjia.cn.interf.ApiUiUpdateListener;
+import com.jianfanjia.cn.tools.JsonParser;
+import com.jianfanjia.cn.tools.LogTool;
+import com.jianfanjia.cn.view.baseview.HorizontalDividerItemDecoration;
 
 /**
  * @author fengliang
@@ -12,17 +23,48 @@ import com.jianfanjia.cn.base.BaseFragment;
  * @Description: 作品
  * @date 2015-8-26 下午1:07:52
  */
-public class ProductFragment extends BaseFragment {
+public class ProductFragment extends BaseFragment implements ApiUiUpdateListener {
     private static final String TAG = DecorationImgFragment.class.getName();
     private RecyclerView prodtct_listview = null;
+    private ProductAdapter productAdapter = null;
 
     @Override
     public void initView(View view) {
         prodtct_listview = (RecyclerView) view.findViewById(R.id.prodtct_listview);
+        prodtct_listview = (RecyclerView) view.findViewById(R.id.my_favorite_designer_listview);
+        prodtct_listview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        prodtct_listview.setItemAnimator(new DefaultItemAnimator());
+        Paint paint = new Paint();
+        paint.setStrokeWidth(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics()));
+        paint.setAlpha(0);
+        paint.setAntiAlias(true);
+        prodtct_listview.addItemDecoration(new HorizontalDividerItemDecoration.Builder(getActivity()).paint(paint).showLastDivider().build());
+        JianFanJiaClient.getCollectListByUser(getActivity(), 0, 100, this, this);
     }
 
     @Override
     public void setListener() {
+
+    }
+
+    @Override
+    public void preLoad() {
+
+    }
+
+    @Override
+    public void loadSuccess(Object data) {
+        LogTool.d(TAG, "data=" + data.toString());
+        ProductInfo productInfo = JsonParser.jsonToBean(data.toString(), ProductInfo.class);
+        LogTool.d(TAG, "productInfo=" + productInfo);
+        if (productInfo != null) {
+            productAdapter = new ProductAdapter(getActivity(), productInfo.getProducts());
+            prodtct_listview.setAdapter(productAdapter);
+        }
+    }
+
+    @Override
+    public void loadFailture(String error_msg) {
 
     }
 
