e9897de21a98c4419d8de42b82e51d9775538369
feng0413 <041332feng@163.com>
2015-12-14 17:09:34 +0800

    commit build

---
+++ app/src/main/java/com/jianfanjia/cn/view/MyDialog.java
@@ -0,0 +1,22 @@
+package com.jianfanjia.cn.view;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ * Name: MyDialog
+ * User: fengliang
+ * Date: 2015-12-14
+ * Time: 17:09
+ */
+public class MyDialog extends Dialog {
+
+    public MyDialog(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+
+    public MyDialog(Context context, int theme) {
+        super(context, theme);
+    }
+}  
