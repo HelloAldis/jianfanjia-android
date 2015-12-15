aa533009d9e01161c8746bf76e33ae993c26e255
feng0413 <041332feng@163.com>
2015-12-15 17:18:00 +0800

    commit build

--- app/src/main/java/com/jianfanjia/cn/activity/PreviewDecorationActivity.java
+++ app/src/main/java/com/jianfanjia/cn/activity/PreviewDecorationActivity.java
@@ -1,11 +1,6 @@
 package com.jianfanjia.cn.activity;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import com.jianfanjia.cn.base.BaseActivity;
 
 /**
  * Description:预览装修美图
@@ -13,23 +8,20 @@ import android.view.View;
  * Email：leo.feng@myjyz.com
  * Date:15-10-11 14:30
  */
-public class PreviewDecorationActivity extends AppCompatActivity {
+public class PreviewDecorationActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_preview_decoration);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public void initView() {
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_preview_decoration;
+    }
 }
