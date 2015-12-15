a367558103f865bdef82d07d47eb0a21b10d118f
feng0413 <041332feng@163.com>
2015-12-15 12:02:49 +0800

    commit build

---
+++ app/src/main/java/com/jianfanjia/cn/view/DecorationPopWindow.java
@@ -0,0 +1,79 @@
+package com.jianfanjia.cn.view;
+
+import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.GridView;
+import android.widget.PopupWindow;
+
+import com.jianfanjia.cn.activity.R;
+import com.jianfanjia.cn.adapter.PopWindowAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Name: DecorationPopWindow
+ * User: fengliang
+ * Date: 2015-12-14
+ * Time: 15:56
+ */
+public class DecorationPopWindow extends PopupWindow {
+    private LayoutInflater inflater = null;
+    private View popView = null;
+    private GridView gridView = null;
+    private PopWindowAdapter adapter = null;
+    private WindowManager.LayoutParams lp = null;
+    private Window window = null;
+
+    public DecorationPopWindow(Activity activity) {
+        super(activity);
+        inflater = LayoutInflater.from(activity);
+        popView = inflater.inflate(R.layout.gird_item_pop, null);
+        gridView = (GridView) popView.findViewById(R.id.popGridview);
+        adapter = new PopWindowAdapter(activity, initData());
+        gridView.setAdapter(adapter);
+        this.setContentView(popView);
+        this.setWidth(LayoutParams.MATCH_PARENT);
+        // 设置SelectPicPopupWindow弹出窗体的高
+        this.setHeight(LayoutParams.WRAP_CONTENT);
+        // 设置SelectPicPopupWindow弹出窗体可点击
+        this.setFocusable(true);
+        // 设置SelectPicPopupWindow弹出窗体动画效果
+//        this.setAnimationStyle(R.style.popwindow_anim);
+        // 实例化一个ColorDrawable颜色为半透明
+        ColorDrawable dw = new ColorDrawable(0x80000000);
+        // 设置SelectPicPopupWindow弹出窗体的背景
+        this.setBackgroundDrawable(dw);
+        // 产生背景变暗效果
+        lp = activity.getWindow().getAttributes();
+        window = activity.getWindow();
+        this.setOutsideTouchable(true);
+        this.setFocusable(true);
+        this.setOnDismissListener(new OnDismissListener() {
+
+            // 在dismiss中恢复透明度
+            @Override
+            public void onDismiss() {
+                lp.alpha = 1f;
+                window.setAttributes(lp);
+            }
+        });
+    }
+
+    private List<String> initData() {
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            list.add("title" + i);
+        }
+        return list;
+    }
+
+    public void show(View view) {
+        showAsDropDown(view, 0, 0);
+    }
+}
