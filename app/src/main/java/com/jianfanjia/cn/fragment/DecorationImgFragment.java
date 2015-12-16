d44250135d1f7c3021749911b8338d378909fd21
feng0413 <041332feng@163.com>
2015-12-16 14:57:40 +0800

    commit build

--- app/src/main/java/com/jianfanjia/cn/fragment/DecorationImgFragment.java
+++ app/src/main/java/com/jianfanjia/cn/fragment/DecorationImgFragment.java
@@ -7,6 +7,10 @@ import android.view.View;
 
 import com.jianfanjia.cn.activity.R;
 import com.jianfanjia.cn.base.BaseFragment;
+import com.jianfanjia.cn.http.JianFanJiaClient;
+import com.jianfanjia.cn.interf.ApiUiUpdateListener;
+import com.jianfanjia.cn.tools.LogTool;
+import com.jianfanjia.cn.view.baseview.SpacesItemDecoration;
 
 /**
  * @author fengliang
@@ -23,12 +27,37 @@ public class DecorationImgFragment extends BaseFragment {
         decoration_img_listview = (RecyclerView) view.findViewById(R.id.decoration_img_listview);
         decoration_img_listview.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
         decoration_img_listview.setItemAnimator(new DefaultItemAnimator());
+        SpacesItemDecoration decoration = new SpacesItemDecoration(10);
+        decoration_img_listview.addItemDecoration(decoration);
+        getDecorationImgList(0, 8, getDecorationImgListListener);
     }
 
     @Override
     public void setListener() {
 
     }
+
+
+    private void getDecorationImgList(int from, int limit, ApiUiUpdateListener listener) {
+        JianFanJiaClient.getBeautyImgListByUser(getActivity(), from, limit, listener, this);
+    }
+
+    private ApiUiUpdateListener getDecorationImgListListener = new ApiUiUpdateListener() {
+        @Override
+        public void preLoad() {
+
+        }
+
+        @Override
+        public void loadSuccess(Object data) {
+            LogTool.d(TAG, "data:" + data.toString());
+        }
+
+        @Override
+        public void loadFailture(String error_msg) {
+
+        }
+    };
 
     @Override
     public int getLayoutId() {
