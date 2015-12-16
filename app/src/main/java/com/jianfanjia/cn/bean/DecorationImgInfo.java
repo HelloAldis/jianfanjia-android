6a32d83c888550e8948049733b56adc502006d37
feng0413 <041332feng@163.com>
2015-12-16 11:47:38 +0800

    commit build

---
+++ app/src/main/java/com/jianfanjia/cn/bean/DecorationImgInfo.java
@@ -0,0 +1,187 @@
+package com.jianfanjia.cn.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Name: DecorationImgInfo
+ * User: fengliang
+ * Date: 2015-12-16
+ * Time: 11:42
+ */
+public class DecorationImgInfo implements Serializable {
+    private String _id;
+    private long lastupdate;
+    private long create_at;
+    private String title;
+    private String description;
+    private String dec_type;
+    private String house_type;
+    private String dec_style;
+    private String status;
+    private String authorid;
+    private String usertype;
+    private int __v;
+    private int favorite_count;
+    private int view_count;
+    private boolean is_my_favorite;
+    private List<Img> images;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public long getLastupdate() {
+        return lastupdate;
+    }
+
+    public void setLastupdate(long lastupdate) {
+        this.lastupdate = lastupdate;
+    }
+
+    public long getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(long create_at) {
+        this.create_at = create_at;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDec_type() {
+        return dec_type;
+    }
+
+    public void setDec_type(String dec_type) {
+        this.dec_type = dec_type;
+    }
+
+    public String getHouse_type() {
+        return house_type;
+    }
+
+    public void setHouse_type(String house_type) {
+        this.house_type = house_type;
+    }
+
+    public String getDec_style() {
+        return dec_style;
+    }
+
+    public void setDec_style(String dec_style) {
+        this.dec_style = dec_style;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAuthorid() {
+        return authorid;
+    }
+
+    public void setAuthorid(String authorid) {
+        this.authorid = authorid;
+    }
+
+    public String getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+
+    public int get__v() {
+        return __v;
+    }
+
+    public void set__v(int __v) {
+        this.__v = __v;
+    }
+
+    public int getFavorite_count() {
+        return favorite_count;
+    }
+
+    public void setFavorite_count(int favorite_count) {
+        this.favorite_count = favorite_count;
+    }
+
+    public int getView_count() {
+        return view_count;
+    }
+
+    public void setView_count(int view_count) {
+        this.view_count = view_count;
+    }
+
+    public boolean is_my_favorite() {
+        return is_my_favorite;
+    }
+
+    public void setIs_my_favorite(boolean is_my_favorite) {
+        this.is_my_favorite = is_my_favorite;
+    }
+
+    public List<Img> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Img> images) {
+        this.images = images;
+    }
+
+    public static class Img implements Serializable {
+        private String imageid;
+        private int width;
+        private int height;
+
+        public String getImageid() {
+            return imageid;
+        }
+
+        public void setImageid(String imageid) {
+            this.imageid = imageid;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public void setWidth(int width) {
+            this.width = width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+    }
+}  
