013976ee89fdadd9b1f197ad7ec103707a1fae53
feng0413 <041332feng@163.com>
2015-12-17 18:57:01 +0800

    commit build

--- app/src/main/java/com/jianfanjia/cn/activity/DesignerCaseInfoActivity.java
+++ app/src/main/java/com/jianfanjia/cn/activity/DesignerCaseInfoActivity.java
@@ -203,7 +203,6 @@ public class DesignerCaseInfoActivity extends BaseActivity implements OnClickLis
         }
     };
 
-
     private ApiUiUpdateListener addProductHomePageInfoListener = new ApiUiUpdateListener() {
         @Override
         public void preLoad() {
