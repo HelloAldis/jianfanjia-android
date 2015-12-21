fe232deec1b912e1d34ac2a73f6c9bfd3525990d
feng0413 <041332feng@163.com>
2015-12-21 14:35:25 +0800

    commit build

--- app/src/main/java/com/jianfanjia/cn/adapter/DelayNotifyAdapter.java
+++ app/src/main/java/com/jianfanjia/cn/adapter/DelayNotifyAdapter.java
@@ -2,10 +2,12 @@ package com.jianfanjia.cn.adapter;
 
 import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.jianfanjia.cn.activity.R;
-import com.jianfanjia.cn.adapter.base.BaseListAdapter;
+import com.jianfanjia.cn.adapter.base.BaseRecyclerViewAdapter;
+import com.jianfanjia.cn.adapter.base.RecyclerViewHolderBase;
 import com.jianfanjia.cn.application.MyApplication;
 import com.jianfanjia.cn.bean.NotifyDelayInfo;
 import com.jianfanjia.cn.config.Constant;
@@ -20,70 +22,47 @@ import java.util.List;
  * @class DelayNotifyAdapter
  * @date 2015-8-26 15:57
  */
-public class DelayNotifyAdapter extends BaseListAdapter<NotifyDelayInfo> {
+public class DelayNotifyAdapter extends BaseRecyclerViewAdapter<NotifyDelayInfo> {
     private DelayInfoListener listener;
 
-    public DelayNotifyAdapter(Context context, List<NotifyDelayInfo> delayList) {
-        super(context, delayList);
-    }
-
-    public DelayNotifyAdapter(Context context, List<NotifyDelayInfo> delayList,
-                              DelayInfoListener listener) {
-        super(context, delayList);
+    public DelayNotifyAdapter(Context context, List<NotifyDelayInfo> list, DelayInfoListener listener) {
+        super(context, list);
         this.listener = listener;
     }
 
     @Override
-    public View initView(final int position, View convertView) {
-        ViewHolder viewHolder = null;
+    public void bindView(RecyclerViewHolderBase viewHolder, final int position, List<NotifyDelayInfo> list) {
         NotifyDelayInfo info = list.get(position);
-        if (convertView == null) {
-            convertView = layoutInflater.inflate(R.layout.list_item_tip_delay,
-                    null);
-            viewHolder = new ViewHolder();
-            viewHolder.itemCellView = (TextView) convertView
-                    .findViewById(R.id.list_item_tip_cell_name);
-            viewHolder.itemNodeView = (TextView) convertView
-                    .findViewById(R.id.list_item_tip_delay_node);
-            viewHolder.itemNewTimeView = (TextView) convertView
-                    .findViewById(R.id.list_item_tip_delay_new_time);
-            viewHolder.itemPubTimeView = (TextView) convertView
-                    .findViewById(R.id.list_item_tip_delay_time);
-            viewHolder.itemStatusView = (TextView) convertView
-                    .findViewById(R.id.list_item_tip_delay_status);
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
+        DelayNotifyViewHolder holder = (DelayNotifyViewHolder) viewHolder;
         final String role = info.getRequest_role();
-        viewHolder.itemCellView.setText(info.getProcess().getCell());
-        viewHolder.itemNodeView.setText(MyApplication.getInstance()
+        holder.itemCellView.setText(info.getProcess().getCell());
+        holder.itemNodeView.setText(MyApplication.getInstance()
                 .getStringById(info.getSection()) + "阶段");
         if (role.equals(Constant.IDENTITY_OWNER)) {
-            viewHolder.itemNewTimeView.setText("您已申请改期验收至" + DateFormatTool.longToString(info.getNew_date()));
+            holder.itemNewTimeView.setText("您已申请改期验收至" + DateFormatTool.longToString(info.getNew_date()));
         } else {
-            viewHolder.itemNewTimeView.setText("对方已申请改期验收至" + DateFormatTool.longToString(info.getNew_date()));
+            holder.itemNewTimeView.setText("对方已申请改期验收至" + DateFormatTool.longToString(info.getNew_date()));
         }
-        viewHolder.itemPubTimeView.setText(DateFormatTool.toLocalTimeString(info.getRequest_date()));
+        holder.itemPubTimeView.setText(DateFormatTool.toLocalTimeString(info.getRequest_date()));
         final String status = info.getStatus();
         if (status.equals(Constant.NO_START)) {
-            viewHolder.itemStatusView.setText("未开工");
+            holder.itemStatusView.setText("未开工");
         } else if (status.equals(Constant.DOING)) {
-            viewHolder.itemStatusView.setText("进行中");
+            holder.itemStatusView.setText("进行中");
         } else if (status.equals(Constant.FINISHED)) {
-            viewHolder.itemStatusView.setText("已完成");
+            holder.itemStatusView.setText("已完成");
         } else if (status.equals(Constant.YANQI_BE_DOING)) {
             if (role.equals(Constant.IDENTITY_OWNER)) {
-                viewHolder.itemStatusView.setText("等待对方确认");
+                holder.itemStatusView.setText("等待对方确认");
             } else {
-                viewHolder.itemStatusView.setText("未处理,点击前往处理");
+                holder.itemStatusView.setText("未处理,点击前往处理");
             }
         } else if (status.equals(Constant.YANQI_AGREE)) {
-            viewHolder.itemStatusView.setText("已同意");
+            holder.itemStatusView.setText("已同意");
         } else if (status.equals(Constant.YANQI_REFUSE)) {
-            viewHolder.itemStatusView.setText("已拒绝");
+            holder.itemStatusView.setText("已拒绝");
         }
-        viewHolder.itemStatusView.setOnClickListener(new View.OnClickListener() {
+        holder.itemStatusView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (null != listener) {
@@ -91,16 +70,39 @@ public class DelayNotifyAdapter extends BaseListAdapter<NotifyDelayInfo> {
                 }
             }
         });
-        return convertView;
     }
 
-    private static class ViewHolder {
-        TextView itemCellView;
-        TextView itemNodeView;// 延迟节点
-        TextView itemNewTimeView;// 延期时间
-        TextView itemPubTimeView;// 发布时间
-        TextView itemStatusView;
+    @Override
+    public View createView(ViewGroup viewGroup, int viewType) {
+        View view = layoutInflater.inflate(R.layout.list_item_tip_delay,
+                null);
+        return view;
     }
 
+    @Override
+    public RecyclerViewHolderBase createViewHolder(View view) {
+        return new DelayNotifyViewHolder(view);
+    }
 
+    private static class DelayNotifyViewHolder extends RecyclerViewHolderBase {
+        public TextView itemCellView;
+        public TextView itemNodeView;// 延迟节点
+        public TextView itemNewTimeView;// 延期时间
+        public TextView itemPubTimeView;// 发布时间
+        public TextView itemStatusView;
+
+        public DelayNotifyViewHolder(View itemView) {
+            super(itemView);
+            itemCellView = (TextView) itemView
+                    .findViewById(R.id.list_item_tip_cell_name);
+            itemNodeView = (TextView) itemView
+                    .findViewById(R.id.list_item_tip_delay_node);
+            itemNewTimeView = (TextView) itemView
+                    .findViewById(R.id.list_item_tip_delay_new_time);
+            itemPubTimeView = (TextView) itemView
+                    .findViewById(R.id.list_item_tip_delay_time);
+            itemStatusView = (TextView) itemView
+                    .findViewById(R.id.list_item_tip_delay_status);
+        }
+    }
 }
