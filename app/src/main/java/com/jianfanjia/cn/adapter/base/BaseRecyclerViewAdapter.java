95af97ae4a83b682a3231eb9f597dd8046556698
feng0413 <041332feng@163.com>
2015-12-22 12:09:04 +0800

    commit build

--- app/src/main/java/com/jianfanjia/cn/adapter/base/BaseRecyclerViewAdapter.java
+++ app/src/main/java/com/jianfanjia/cn/adapter/base/BaseRecyclerViewAdapter.java
@@ -62,15 +62,6 @@ public abstract class BaseRecyclerViewAdapter<T> extends RecyclerView.Adapter<Re
         }
     }
 
-    public void remove(List<T> l, int position) {
-        if (list.size() > 0) {
-            for (T t : l) {
-                list.add(t);
-            }
-            notifyItemRangeRemoved(position, list.size());
-        }
-    }
-
     public void remove(int position) {
         list.remove(position);
         notifyItemRemoved(position);
