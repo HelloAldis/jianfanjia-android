2e6a80c3dbaac0fd5b33091ba0f8f8ac30984761
feng0413 <041332feng@163.com>
2016-01-04 14:42:43 +0800

    commit YanQiFragment

--- app/src/main/java/com/jianfanjia/cn/tools/ShareUtil.java
+++ app/src/main/java/com/jianfanjia/cn/tools/ShareUtil.java
@@ -1,5 +1,12 @@
 package com.jianfanjia.cn.tools;
 
+import android.app.Activity;
+
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.media.UMImage;
+
 /**
  * Description: com.jianfanjia.cn.tools
  * Author: zhanghao
@@ -7,4 +14,23 @@ package com.jianfanjia.cn.tools;
  * Date:2015-12-31 14:25
  */
 public class ShareUtil {
+    private static ShareUtil shareUtil;
+    private ShareAction shareAction = null;
+
+    public static ShareUtil getShareUtil(Activity activity) {
+        if (null == shareUtil) {
+            shareUtil = new ShareUtil(activity);
+        }
+        return shareUtil;
+    }
+
+    public ShareUtil(Activity activity) {
+        shareAction = new ShareAction(activity);
+    }
+
+
+    public void share(Activity activity, String url, SHARE_MEDIA platform, UMShareListener umShareListener) {
+        UMImage image = new UMImage(activity, url);
+        shareAction.setPlatform(platform).setCallback(umShareListener).withText("分享美图").withMedia(image).share();
+    }
 }
