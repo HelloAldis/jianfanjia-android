63e970bb837472913e55ddd3aed426df5c6e6487
feng0413 <041332feng@163.com>
2016-01-04 13:58:22 +0800

    commit YanQiFragment

---
+++ app/src/main/java/com/jianfanjia/cn/interf/ShowPopWindowCallBack.java
@@ -0,0 +1,12 @@
+package com.jianfanjia.cn.interf;
+
+/**
+ * Created by Administrator on 2016/1/4.
+ */
+public interface ShowPopWindowCallBack {
+    void shareWeiXin();
+
+    void shareWeiBo();
+
+    void shareQQ();
+}
