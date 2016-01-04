a336bd4930142bbcd405f4f22dabf412d036ea98
feng0413 <041332feng@163.com>
2016-01-04 14:28:47 +0800

    commit YanQiFragment

--- app/src/main/java/com/jianfanjia/cn/application/MyApplication.java
+++ app/src/main/java/com/jianfanjia/cn/application/MyApplication.java
@@ -40,6 +40,7 @@ public class MyApplication extends BaseApplication {
 
         PlatformConfig.setWeixin("wx391daabfce27e728", "f7c8e3e1b5910dd93be2744dacb3a1cc");
         PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad");
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
 
         /*
          * Thread.setDefaultUncaughtExceptionHandler(AppException
