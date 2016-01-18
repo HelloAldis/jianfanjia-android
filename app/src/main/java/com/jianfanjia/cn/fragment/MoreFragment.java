b2997910b78929943e7d5c6e71931c0b33278170
feng0413 <041332feng@163.com>
2016-01-18 12:17:56 +0800

    commit Constant

--- app/src/main/java/com/jianfanjia/cn/fragment/MoreFragment.java
+++ app/src/main/java/com/jianfanjia/cn/fragment/MoreFragment.java
@@ -1,8 +1,11 @@
 package com.jianfanjia.cn.fragment;
 
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.jianfanjia.cn.activity.R;
+import com.jianfanjia.cn.activity.my.CustomerServiceActivity;
+import com.jianfanjia.cn.activity.my.SettingActivity;
 import com.jianfanjia.cn.base.BaseFragment;
 
 /**
@@ -12,15 +15,52 @@ import com.jianfanjia.cn.base.BaseFragment;
  * Date:15-10-11 14:30
  */
 public class MoreFragment extends BaseFragment {
+    private static final String TAG = MoreFragment.class.getName();
+    private RelativeLayout xuqiuLayout = null;
+    private RelativeLayout collect_layout = null;
+    private RelativeLayout setting_layout = null;
+    private RelativeLayout kefu_layout = null;
+    private RelativeLayout my_info_layout = null;
 
     @Override
     public void initView(View view) {
-
+        xuqiuLayout = (RelativeLayout) view.findViewById(R.id.xuqiu_layout);
+        collect_layout = (RelativeLayout) view.findViewById(R.id.collect_layout);
+        setting_layout = (RelativeLayout) view.findViewById(R.id.setting_layout);
+        my_info_layout = (RelativeLayout) view.findViewById(R.id.frag_my_info_layout);
+        kefu_layout = (RelativeLayout) view.findViewById(R.id.kefu_layout);
     }
 
     @Override
     public void setListener() {
+        xuqiuLayout.setOnClickListener(this);
+        collect_layout.setOnClickListener(this);
+        setting_layout.setOnClickListener(this);
+        my_info_layout.setOnClickListener(this);
+        kefu_layout.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.xuqiu_layout:
+
+                break;
+            case R.id.collect_layout:
+
+                break;
+            case R.id.frag_my_info_layout:
+
+                break;
+            case R.id.kefu_layout:
+                startActivity(CustomerServiceActivity.class);
+                break;
+            case R.id.setting_layout:
+                startActivity(SettingActivity.class);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
