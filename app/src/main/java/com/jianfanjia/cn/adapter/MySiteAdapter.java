556c5321bbbddb0d0356e5761873a4a08fdbe62b
feng0413 <041332feng@163.com>
2016-01-18 14:39:31 +0800

    commit Constant

---
+++ app/src/main/java/com/jianfanjia/cn/adapter/MySiteAdapter.java
@@ -0,0 +1,67 @@
+package com.jianfanjia.cn.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.jianfanjia.cn.activity.R;
+import com.jianfanjia.cn.adapter.base.BaseRecyclerViewAdapter;
+import com.jianfanjia.cn.adapter.base.RecyclerViewHolderBase;
+import com.jianfanjia.cn.bean.MySiteInfo;
+
+import java.util.List;
+
+/**
+ * Name: MySiteAdapter
+ * User: fengliang
+ * Date: 2016-01-18
+ * Time: 14:21
+ */
+public class MySiteAdapter extends BaseRecyclerViewAdapter<MySiteInfo> {
+
+    public MySiteAdapter(Context context, List<MySiteInfo> list) {
+        super(context, list);
+    }
+
+    @Override
+    public void bindView(RecyclerViewHolderBase viewHolder, int position, List<MySiteInfo> list) {
+        MySiteInfo info = list.get(position);
+    }
+
+    @Override
+    public View createView(ViewGroup viewGroup, int viewType) {
+        View view = layoutInflater.inflate(R.layout.list_item_site_info,
+                null);
+        return view;
+    }
+
+    @Override
+    public RecyclerViewHolderBase createViewHolder(View view) {
+        return new MySiteViewHolder(view);
+    }
+
+    private static class MySiteViewHolder extends RecyclerViewHolderBase {
+        public ImageView itemHeadView;
+        public TextView itemCellView;
+        public TextView itemNodeView;
+        public RecyclerView item_process_listview;
+        public TextView itemPubTimeView;
+        public TextView itemContractView;
+        public TextView itemPlanView;
+
+
+        public MySiteViewHolder(View itemView) {
+            super(itemView);
+            itemHeadView = (ImageView) itemView.findViewById(R.id.list_item_head_img);
+            itemCellView = (TextView) itemView.findViewById(R.id.cellText);
+            itemNodeView = (TextView) itemView.findViewById(R.id.nodeText);
+            item_process_listview = (RecyclerView) itemView.findViewById(R.id.item_site_section_listview);
+            itemPubTimeView = (TextView) itemView.findViewById(R.id.dateText);
+            itemContractView = (TextView) itemView.findViewById(R.id.contractText);
+            itemPlanView = (TextView) itemView.findViewById(R.id.planText);
+        }
+    }
+}
--- app/src/main/java/com/jianfanjia/cn/fragment/ManageFragment.java
+++ app/src/main/java/com/jianfanjia/cn/fragment/ManageFragment.java
@@ -1,9 +1,19 @@
 package com.jianfanjia.cn.fragment;
 
+import android.graphics.Paint;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.View;
 
 import com.jianfanjia.cn.activity.R;
+import com.jianfanjia.cn.adapter.MySiteAdapter;
 import com.jianfanjia.cn.base.BaseFragment;
+import com.jianfanjia.cn.view.MainHeadView;
+import com.jianfanjia.cn.view.baseview.HorizontalDividerItemDecoration;
+import com.jianfanjia.cn.view.library.PullToRefreshBase;
+import com.jianfanjia.cn.view.library.PullToRefreshRecycleView;
 
 /**
  * Description:工地管理
@@ -11,17 +21,47 @@ import com.jianfanjia.cn.base.BaseFragment;
  * Email：leo.feng@myjyz.com
  * Date:15-10-11 14:30
  */
-public class ManageFragment extends BaseFragment {
-
+public class ManageFragment extends BaseFragment implements PullToRefreshBase.OnRefreshListener2<RecyclerView> {
+    private static final String TAG = ManageFragment.class.getName();
+    private MainHeadView mainHeadView = null;
+    private PullToRefreshRecycleView manage_pullfefresh = null;
+    private MySiteAdapter mySiteAdapter = null;
 
     @Override
     public void initView(View view) {
+        initMainHeadView(view);
+        manage_pullfefresh = (PullToRefreshRecycleView) view.findViewById(R.id.manage_pullfefresh);
+        manage_pullfefresh.setMode(PullToRefreshBase.Mode.BOTH);
+        manage_pullfefresh.setLayoutManager(new LinearLayoutManager(getActivity()));
+        manage_pullfefresh.setItemAnimator(new DefaultItemAnimator());
+        Paint paint = new Paint();
+        paint.setStrokeWidth(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics()));
+        paint.setAlpha(0);
+        paint.setAntiAlias(true);
+        manage_pullfefresh.addItemDecoration(new HorizontalDividerItemDecoration.Builder(getActivity()).paint(paint).showLastDivider().build());
+    }
 
+    private void initMainHeadView(View view) {
+        mainHeadView = (MainHeadView) view.findViewById(R.id.manage_head);
+        mainHeadView.setMianTitle("我的工地");
+        mainHeadView.setBackgroundTransparent();
+        mainHeadView.setRightTitleVisable(View.GONE);
+        mainHeadView.setBackLayoutVisable(View.GONE);
     }
 
     @Override
     public void setListener() {
+        manage_pullfefresh.setOnRefreshListener(this);
+    }
 
+    @Override
+    public void onPullDownToRefresh(PullToRefreshBase<RecyclerView> refreshView) {
+        manage_pullfefresh.onRefreshComplete();
+    }
+
+    @Override
+    public void onPullUpToRefresh(PullToRefreshBase<RecyclerView> refreshView) {
+        manage_pullfefresh.onRefreshComplete();
     }
 
     @Override
