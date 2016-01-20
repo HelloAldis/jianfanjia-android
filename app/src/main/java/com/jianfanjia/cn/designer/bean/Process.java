02bd587accbb21b84527ec66d1d251c97de22293
feng0413 <041332feng@163.com>
2016-01-20 09:53:27 +0800

    commit MySiteAdapter

--- app/src/main/java/com/jianfanjia/cn/designer/bean/Process.java
+++ app/src/main/java/com/jianfanjia/cn/designer/bean/Process.java
@@ -3,94 +3,96 @@ package com.jianfanjia.cn.designer.bean;
 import java.io.Serializable;
 
 /**
- *
+ * @author zhanghao
  * @ClassName: DesignerSiteInfo
  * @Description: 工地基本信息
- * @author zhanghao
  * @date 2015-8-26 下午20:03
- *
  */
 public class Process implements Serializable {
 
-	private static final long serialVersionUID = -5635939585005504702L;
-	private String _id;// 工地id
+    private static final long serialVersionUID = -5635939585005504702L;
+    private String _id;// 工地id
 
-	private String city;
+    private String city;
 
-	private String district;
+    private String district;
 
-	private String cell;// 小区名称
+    private String cell;// 小区名称
 
-	private String userid;// 业主id
+    private String userid;// 业主id
 
-	private String going_on;// 所处阶段
+    private String going_on;// 所处阶段
 
-	private String final_designerid;//设计师id
+    private String final_designerid;//设计师id
 
-	private User user;
+    private String final_planid;
 
-	public String getFinal_designerid() {
-		return final_designerid;
-	}
+    private String requirementid;
 
-	public void setFinal_designerid(String final_designerid) {
-		this.final_designerid = final_designerid;
-	}
+    private User user;
 
-	public String getUserid() {
-		return userid;
-	}
+    public String getFinal_designerid() {
+        return final_designerid;
+    }
 
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
+    public void setFinal_designerid(String final_designerid) {
+        this.final_designerid = final_designerid;
+    }
 
-	public String getDistrict() {
-		return district;
-	}
+    public String getUserid() {
+        return userid;
+    }
 
-	public void setDistrict(String district) {
-		this.district = district;
-	}
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
 
-	public String getCell() {
-		return cell;
-	}
+    public String getDistrict() {
+        return district;
+    }
 
-	public void setCell(String cell) {
-		this.cell = cell;
-	}
+    public void setDistrict(String district) {
+        this.district = district;
+    }
 
-	public String getCity() {
-		return city;
-	}
+    public String getCell() {
+        return cell;
+    }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+    public void setCell(String cell) {
+        this.cell = cell;
+    }
 
-	public String get_id() {
-		return _id;
-	}
+    public String getCity() {
+        return city;
+    }
 
-	public void set_id(String _id) {
-		this._id = _id;
-	}
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	public String getGoing_on() {
-		return going_on;
-	}
+    public String get_id() {
+        return _id;
+    }
 
-	public void setGoing_on(String going_on) {
-		this.going_on = going_on;
-	}
+    public void set_id(String _id) {
+        this._id = _id;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    public String getGoing_on() {
+        return going_on;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public void setGoing_on(String going_on) {
+        this.going_on = going_on;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
 }
