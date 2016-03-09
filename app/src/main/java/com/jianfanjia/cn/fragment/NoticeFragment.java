92b1eb0d5f059b613c06ac3ee192ea37b3d6b6bc
feng0413 <041332feng@163.com>
2016-03-09 10:14:44 +0800

    local changes

--- app/src/main/java/com/jianfanjia/cn/fragment/NoticeFragment.java
+++ app/src/main/java/com/jianfanjia/cn/fragment/NoticeFragment.java
@@ -4,15 +4,21 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
+import com.jianfanjia.cn.Event.MessageEvent;
 import com.jianfanjia.cn.activity.R;
-import com.jianfanjia.cn.base.BaseFragment;
+import com.jianfanjia.cn.config.Constant;
+import com.jianfanjia.cn.interf.ApiUiUpdateListener;
 import com.jianfanjia.cn.tools.LogTool;
 import com.jianfanjia.cn.tools.UiHelper;
 import com.jianfanjia.cn.view.library.PullToRefreshBase;
 import com.jianfanjia.cn.view.library.PullToRefreshRecycleView;
+
+import de.greenrobot.event.EventBus;
 
 /**
  * @author fengliang
@@ -20,12 +26,16 @@ import com.jianfanjia.cn.view.library.PullToRefreshRecycleView;
  * @Description: 通知 全部
  * @date 2015-8-26 下午1:07:52
  */
-public class NoticeFragment extends BaseFragment implements PullToRefreshBase.OnRefreshListener2<RecyclerView> {
+public class NoticeFragment extends CommonFragment implements PullToRefreshBase.OnRefreshListener2<RecyclerView> {
     private static final String TAG = NoticeFragment.class.getName();
+    private View view = null;
     private PullToRefreshRecycleView all_notice_listview = null;
     private RelativeLayout emptyLayout = null;
     private RelativeLayout errorLayout = null;
 
+    private boolean isPrepared = false;
+    private boolean mHasLoadedOnce = false;
+    private int FROM = 0;
     private int mType = -1;
 
     public static NoticeFragment newInstance(int type) {
@@ -39,12 +49,28 @@ public class NoticeFragment extends BaseFragment implements PullToRefreshBase.On
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EventBus.getDefault().register(this);
         mType = getArguments().getInt("Type");
-        LogTool.d(TAG, "mType=============================" + mType);
+        LogTool.d(TAG, "mType=" + mType);
     }
 
     @Override
-    public void initView(View view) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        LogTool.d(TAG, "=========onCreateView()");
+        if (null == view) {
+            view = inflater.inflate(R.layout.fragment_all_notice, container, false);
+            initView();
+            isPrepared = true;
+            load();
+        }
+        ViewGroup parent = (ViewGroup) view.getParent();
+        if (null != parent) {
+            parent.removeView(view);
+        }
+        return view;
+    }
+
+    public void initView() {
         emptyLayout = (RelativeLayout) view.findViewById(R.id.empty_include);
         errorLayout = (RelativeLayout) view.findViewById(R.id.error_include);
         all_notice_listview = (PullToRefreshRecycleView) view.findViewById(R.id.all_notice_listview);
@@ -56,8 +82,32 @@ public class NoticeFragment extends BaseFragment implements PullToRefreshBase.On
     }
 
     @Override
+    protected void load() {
+        if (!isPrepared || !isVisible || mHasLoadedOnce) {
+            return;
+        }
+        getNoticeList(FROM, Constant.HOME_PAGE_LIMIT, pullDownListener);
+    }
+
+    @Override
     public void setListener() {
         all_notice_listview.setOnRefreshListener(this);
+        errorLayout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.error_include:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void getNoticeList(int from, int limit, ApiUiUpdateListener listener) {
+
     }
 
     @Override
@@ -70,18 +120,49 @@ public class NoticeFragment extends BaseFragment implements PullToRefreshBase.On
         all_notice_listview.onRefreshComplete();
     }
 
-    private void getNoticeList() {
+    private ApiUiUpdateListener pullDownListener = new ApiUiUpdateListener() {
+        @Override
+        public void preLoad() {
+
+        }
+
+        @Override
+        public void loadSuccess(Object data) {
+            LogTool.d(TAG, "data:" + data.toString());
+
+        }
+
+        @Override
+        public void loadFailture(String error_msg) {
+            makeTextShort(error_msg);
+        }
+    };
+
+    private ApiUiUpdateListener pullUpListener = new ApiUiUpdateListener() {
+        @Override
+        public void preLoad() {
+
+        }
+
+        @Override
+        public void loadSuccess(Object data) {
+            LogTool.d(TAG, "data:" + data.toString());
+
+        }
+
+        @Override
+        public void loadFailture(String error_msg) {
+            makeTextLong(error_msg);
+        }
+    };
+
+    public void onEventMainThread(MessageEvent event) {
 
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        EventBus.getDefault().unregister(this);
     }
-
-    @Override
-    public int getLayoutId() {
-        return R.layout.fragment_all_notice;
-    }
-
 }
