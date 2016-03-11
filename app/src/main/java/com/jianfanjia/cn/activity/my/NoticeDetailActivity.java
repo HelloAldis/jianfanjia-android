81953480027831a1a3ac4e12c468e070d37fe7dc
feng0413 <041332feng@163.com>
2016-03-11 18:21:35 +0800

    local changes

--- app/src/main/java/com/jianfanjia/cn/activity/my/NoticeDetailActivity.java
+++ app/src/main/java/com/jianfanjia/cn/activity/my/NoticeDetailActivity.java
@@ -28,7 +28,7 @@ import com.jianfanjia.cn.view.MainHeadView;
  * Email：leo.feng@myjyz.com
  * Date:15-10-11 14:30
  */
-public class NoticeDetailActivity extends SwipeBackActivity implements View.OnClickListener, ApiUiUpdateListener {
+public class NoticeDetailActivity extends SwipeBackActivity implements View.OnClickListener {
     private static final String TAG = NoticeDetailActivity.class.getName();
     private MainHeadView mainHeadView = null;
     private TextView typeText = null;
@@ -36,12 +36,16 @@ public class NoticeDetailActivity extends SwipeBackActivity implements View.OnCl
     private TextView sectionText = null;
     private TextView dateText = null;
     private WebView contentView = null;
-    private LinearLayout doubleBtnLayout = null;
-    private LinearLayout singleBtnLayout = null;
+    private LinearLayout delayBtnLayout = null;
+    private LinearLayout checkBtnLayout = null;
+    private LinearLayout otherBtnLayout = null;
     private Button btnAgree = null;
     private Button btnReject = null;
     private Button btnConfirm = null;
+    private Button btnOther = null;
     private String messageid = null;
+
+    private String processid = null;
 
     @Override
     public void initView() {
@@ -58,11 +62,13 @@ public class NoticeDetailActivity extends SwipeBackActivity implements View.OnCl
         contentView.getSettings().setJavaScriptEnabled(true);
         contentView.setBackgroundColor(0); // 设置背景色
         contentView.setWebChromeClient(new WebChromeClient());
-        doubleBtnLayout = (LinearLayout) findViewById(R.id.doubleBtnLayout);
-        singleBtnLayout = (LinearLayout) findViewById(R.id.singleBtnLayout);
+        delayBtnLayout = (LinearLayout) findViewById(R.id.delayBtnLayout);
+        checkBtnLayout = (LinearLayout) findViewById(R.id.checkBtnLayout);
+        otherBtnLayout = (LinearLayout) findViewById(R.id.otherBtnLayout);
         btnAgree = (Button) findViewById(R.id.btnAgree);
         btnReject = (Button) findViewById(R.id.btnReject);
         btnConfirm = (Button) findViewById(R.id.btnConfirm);
+        btnOther = (Button) findViewById(R.id.btnOther);
         getNoticeDetailInfo(messageid);
     }
 
@@ -80,11 +86,9 @@ public class NoticeDetailActivity extends SwipeBackActivity implements View.OnCl
         btnAgree.setOnClickListener(this);
         btnReject.setOnClickListener(this);
         btnConfirm.setOnClickListener(this);
+        btnOther.setOnClickListener(this);
     }
 
-    private void getNoticeDetailInfo(String messageid) {
-        JianFanJiaClient.getUserMsgDetail(NoticeDetailActivity.this, messageid, this, this);
-    }
 
     @Override
     public void onClick(View v) {
@@ -93,74 +97,142 @@ public class NoticeDetailActivity extends SwipeBackActivity implements View.OnCl
                 appManager.finishActivity(this);
                 break;
             case R.id.btnAgree:
+                agreeReschedule(processid);
                 break;
             case R.id.btnReject:
+                refuseReschedule(processid);
                 break;
             case R.id.btnConfirm:
+//                Bundle checkBundle = new Bundle();
+//                checkBundle.putString(Constant.PROCESS_NAME, "");
+//                checkBundle.putString(Constant.PROCESS_STATUS, "");
+//                checkBundle.putString(Global.PROCESS_ID, "");
+//                Intent checkIntent = new Intent(NoticeDetailActivity.this, CheckActivity.class);
+//                checkIntent.putExtras(checkBundle);
+                break;
+            case R.id.btnOther:
+                LogTool.d(TAG, "==================================================");
                 break;
             default:
                 break;
         }
     }
 
-    @Override
-    public void preLoad() {
-        showWaitDialog();
-    }
-
-    @Override
-    public void loadSuccess(Object data) {
-        LogTool.d(TAG, "data:" + data.toString());
-        hideWaitDialog();
-        NoticeDetailInfo noticeDetailInfo = JsonParser.jsonToBean(data.toString(), NoticeDetailInfo.class);
-        if (null != noticeDetailInfo) {
-            typeText.setBackgroundResource(R.drawable.detail_text_bg_border);
-            String msgType = noticeDetailInfo.getMessage_type();
-            LogTool.d(TAG, "msgType:" + msgType);
-            if (msgType.equals(Constant.TYPE_DELAY_MSG)) {
-                typeText.setText(getResources().getString(R.string.delay_str));
-                cellText.setText(noticeDetailInfo.getProcess().getCell());
-                sectionText.setVisibility(View.VISIBLE);
-                sectionText.setText(MyApplication.getInstance().getStringById(noticeDetailInfo.getSection()) + "阶段");
-            } else if (msgType.equals(Constant.TYPE_CAIGOU_MSG)) {
-                typeText.setText(getResources().getString(R.string.caigou_str));
-                cellText.setText(noticeDetailInfo.getProcess().getCell());
-                sectionText.setVisibility(View.VISIBLE);
-                sectionText.setText(MyApplication.getInstance().getStringById(noticeDetailInfo.getSection()) + "阶段");
-            } else if (msgType.equals(Constant.TYPE_PAY_MSG)) {
-                typeText.setText(getResources().getString(R.string.pay_str));
-                cellText.setText(noticeDetailInfo.getProcess().getCell());
-                sectionText.setVisibility(View.VISIBLE);
-                sectionText.setText(MyApplication.getInstance().getStringById(noticeDetailInfo.getSection()) + "阶段");
-            } else if (msgType.equals(Constant.TYPE_CONFIRM_CHECK_MSG)) {
-                typeText.setText(getResources().getString(R.string.check_str));
-                cellText.setText(noticeDetailInfo.getProcess().getCell());
-                sectionText.setVisibility(View.VISIBLE);
-                sectionText.setText(MyApplication.getInstance().getStringById(noticeDetailInfo.getSection()) + "阶段");
-            } else if (msgType.equals(Constant.TYPE_SYSTEM_MSG)) {
-                typeText.setText(getResources().getString(R.string.sys_str));
-            } else {
-                typeText.setText(getResources().getString(R.string.req_str));
-                cellText.setText(noticeDetailInfo.getRequirement().getCell());
-                sectionText.setVisibility(View.GONE);
+    //获取详情
+    private void getNoticeDetailInfo(String messageid) {
+        JianFanJiaClient.getUserMsgDetail(NoticeDetailActivity.this, messageid, new ApiUiUpdateListener() {
+            @Override
+            public void preLoad() {
+                showWaitDialog();
             }
-            dateText.setText(DateFormatTool.getRelativeTime(noticeDetailInfo.getCreate_at()));
-            contentView.loadDataWithBaseURL(null, "<html>" + "<body>\n" +
-                    "<font size=\"4\" color=\"#7c8389\">\n" +
-                    "<p>尊敬的业主您好：</p >\n" +
-                    "<p>您的设计师戴涛希望将本阶段工期修改至</p >\n" +
-                    "<p><font size=\"4\" color=\"#fe7003\">2016-04-01</font></p >\n" +
-                    "<p>等待您的确认！如有问题请及时与设计师联系。</p >\n" +
-                    "<p>也可以拨打我们的客服热线：400-8515-167</p >\n" +
-                    "</font>\n" +
-                    "</body>" + "<html>", "text/html", "utf-8", null);
-        }
+
+            @Override
+            public void loadSuccess(Object data) {
+                LogTool.d(TAG, "data:" + data.toString());
+                hideWaitDialog();
+                NoticeDetailInfo noticeDetailInfo = JsonParser.jsonToBean(data.toString(), NoticeDetailInfo.class);
+                if (null != noticeDetailInfo) {
+                    typeText.setBackgroundResource(R.drawable.detail_text_bg_border);
+                    String msgType = noticeDetailInfo.getMessage_type();
+                    LogTool.d(TAG, "msgType:" + msgType);
+                    if (msgType.equals(Constant.TYPE_DELAY_MSG)) {
+                        processid = noticeDetailInfo.getProcessid();
+                        LogTool.d(TAG, "processid=" + processid);
+                        delayBtnLayout.setVisibility(View.VISIBLE);
+                        checkBtnLayout.setVisibility(View.GONE);
+                        otherBtnLayout.setVisibility(View.GONE);
+                        typeText.setText(getResources().getString(R.string.delay_str));
+                        cellText.setText(noticeDetailInfo.getProcess().getCell());
+                        sectionText.setVisibility(View.VISIBLE);
+                        sectionText.setText(MyApplication.getInstance().getStringById(noticeDetailInfo.getSection()) + "阶段");
+                    } else if (msgType.equals(Constant.TYPE_CAIGOU_MSG)) {
+                        typeText.setText(getResources().getString(R.string.caigou_str));
+                        cellText.setText(noticeDetailInfo.getProcess().getCell());
+                        sectionText.setVisibility(View.VISIBLE);
+                        sectionText.setText(MyApplication.getInstance().getStringById(noticeDetailInfo.getSection()) + "阶段");
+                    } else if (msgType.equals(Constant.TYPE_PAY_MSG)) {
+                        typeText.setText(getResources().getString(R.string.pay_str));
+                        cellText.setText(noticeDetailInfo.getProcess().getCell());
+                        sectionText.setVisibility(View.VISIBLE);
+                        sectionText.setText(MyApplication.getInstance().getStringById(noticeDetailInfo.getSection()) + "阶段");
+                    } else if (msgType.equals(Constant.TYPE_CONFIRM_CHECK_MSG)) {
+                        delayBtnLayout.setVisibility(View.GONE);
+                        checkBtnLayout.setVisibility(View.VISIBLE);
+                        otherBtnLayout.setVisibility(View.GONE);
+                        typeText.setText(getResources().getString(R.string.check_str));
+                        cellText.setText(noticeDetailInfo.getProcess().getCell());
+                        sectionText.setVisibility(View.VISIBLE);
+                        sectionText.setText(MyApplication.getInstance().getStringById(noticeDetailInfo.getSection()) + "阶段");
+                    } else if (msgType.equals(Constant.TYPE_SYSTEM_MSG)) {
+                        typeText.setText(getResources().getString(R.string.sys_str));
+                    } else {
+                        delayBtnLayout.setVisibility(View.GONE);
+                        checkBtnLayout.setVisibility(View.GONE);
+                        otherBtnLayout.setVisibility(View.VISIBLE);
+                        typeText.setText(getResources().getString(R.string.req_str));
+                        cellText.setText(noticeDetailInfo.getRequirement().getCell());
+                        sectionText.setVisibility(View.GONE);
+                    }
+                    dateText.setText(DateFormatTool.getRelativeTime(noticeDetailInfo.getCreate_at()));
+                    contentView.loadDataWithBaseURL(null, "<html>" + "<body>\n" +
+                            "<font size=\"4\" color=\"#7c8389\">\n" +
+                            "<p>尊敬的业主您好：</p >\n" +
+                            "<p>您的设计师戴涛希望将本阶段工期修改至</p >\n" +
+                            "<p><font size=\"4\" color=\"#fe7003\">2016-04-01</font></p >\n" +
+                            "<p>等待您的确认！如有问题请及时与设计师联系。</p >\n" +
+                            "<p>也可以拨打我们的客服热线：400-8515-167</p >\n" +
+                            "</font>\n" +
+                            "</body>" + "<html>", "text/html", "utf-8", null);
+                }
+            }
+
+            @Override
+            public void loadFailture(String error_msg) {
+                hideWaitDialog();
+                makeTextShort(error_msg);
+            }
+        }, this);
     }
 
-    @Override
-    public void loadFailture(String error_msg) {
-        hideWaitDialog();
-        makeTextShort(error_msg);
+    //同意改期
+    private void agreeReschedule(String processid) {
+        JianFanJiaClient.agreeReschedule(NoticeDetailActivity.this, processid, new ApiUiUpdateListener() {
+            @Override
+            public void preLoad() {
+
+            }
+
+            @Override
+            public void loadSuccess(Object data) {
+                LogTool.d(TAG, "data:" + data.toString());
+
+            }
+
+            @Override
+            public void loadFailture(String error_msg) {
+                makeTextShort(error_msg);
+            }
+        }, this);
+    }
+
+    // 拒绝改期
+    private void refuseReschedule(String processid) {
+        JianFanJiaClient.refuseReschedule(NoticeDetailActivity.this, processid, new ApiUiUpdateListener() {
+            @Override
+            public void preLoad() {
+
+            }
+
+            @Override
+            public void loadSuccess(Object data) {
+                LogTool.d(TAG, "data:" + data.toString());
+            }
+
+            @Override
+            public void loadFailture(String error_msg) {
+                makeTextShort(error_msg);
+            }
+        }, this);
     }
 
     @Override
