eee66fac70a2467a4dd4926041b77788d0afa48a
feng0413 <041332feng@163.com>
2016-03-25 13:19:02 +0800

    MyNewFragment.java

--- app/src/main/java/com/jianfanjia/cn/fragment/MyNewFragment.java
+++ app/src/main/java/com/jianfanjia/cn/fragment/MyNewFragment.java
@@ -13,8 +13,6 @@ import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-import java.util.List;
-
 import com.google.gson.reflect.TypeToken;
 import com.jianfanjia.cn.activity.R;
 import com.jianfanjia.cn.activity.common.CommentListActivity;
@@ -37,72 +35,90 @@ import com.jianfanjia.cn.view.dialog.CommonDialog;
 import com.jianfanjia.cn.view.dialog.DialogHelper;
 import com.jianfanjia.cn.view.layout.BadgeView;
 
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.OnClick;
+
 /**
  * Description:我的
  * Author：fengliang
  * Email：leo.feng@myjyz.com
  * Date:15-10-11 14:30
  */
-public class MyNewFragment extends BaseFragment implements View.OnClickListener{
+public class MyNewFragment extends BaseFragment {
     private static final String TAG = MyNewFragment.class.getName();
     public static final int REQUESTCODE_USERINFO = 0;
 
-    private RelativeLayout notifyLayout = null;
-    private RelativeLayout my_collect_layout = null;
-    private RelativeLayout setting_layout = null;
-    private RelativeLayout kefu_layout = null;
-    private RelativeLayout my_info_layout = null;
-    private RelativeLayout feedback_layout = null;
-    private RelativeLayout clearCacheLayout = null;
-    private RelativeLayout callPhoneLayout = null;
-    private RelativeLayout commentLayout = null;
-    private RelativeLayout bindingAccountLayout = null;
-    private TextView cacheSizeView = null;
-    private ScrollView scrollView = null;
-    private ImageView head_img = null;
-    private ImageView user_head_img = null;
-    private TextView my_name = null;
-    private TextView my_account = null;
-    public BadgeView noticeCountView = null;
-    public BadgeView commentCountView = null;
+    @Bind(R.id.notify_layout)
+    RelativeLayout notifyLayout;
+
+    @Bind(R.id.collect_layout)
+    RelativeLayout my_collect_layout;
+
+    @Bind(R.id.setting_layout)
+    RelativeLayout setting_layout;
+
+    @Bind(R.id.kefu_layout)
+    RelativeLayout kefu_layout;
+
+    @Bind(R.id.frag_my_info_layout)
+    RelativeLayout my_info_layout;
+
+    @Bind(R.id.feedback_layout)
+    RelativeLayout feedback_layout;
+
+    @Bind(R.id.clear_cache_layout)
+    RelativeLayout clearCacheLayout;
+
+    @Bind(R.id.call_layout)
+    RelativeLayout callPhoneLayout;
+
+    @Bind(R.id.comment_layout)
+    RelativeLayout commentLayout;
+
+    @Bind(R.id.binding_account_layout)
+    RelativeLayout bindingAccountLayout;
+
+    @Bind(R.id.cache_size)
+    TextView cacheSizeView;
+
+    @Bind(R.id.setting_scrollview)
+    ScrollView scrollView;
+
+    @Bind(R.id.head_img)
+    ImageView head_img;
+
+    @Bind(R.id.user_head_img)
+    ImageView user_head_img;
+
+    @Bind(R.id.frag_my_name)
+    TextView my_name;
+
+    @Bind(R.id.frag_my_account)
+    TextView my_account;
+
+    @Bind(R.id.notify_count_text)
+    public BadgeView noticeCountView;
+
+    @Bind(R.id.comment_count_text)
+    public BadgeView commentCountView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view =  super.onCreateView(inflater, container, savedInstanceState);
-        initView(view);
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        initView();
         setListener();
         return view;
     }
 
-    public void initView(View view) {
-        notifyLayout = (RelativeLayout) view.findViewById(R.id.notify_layout);
-        my_collect_layout = (RelativeLayout) view.findViewById(R.id.collect_layout);
-        setting_layout = (RelativeLayout) view.findViewById(R.id.setting_layout);
-        my_info_layout = (RelativeLayout) view.findViewById(R.id.frag_my_info_layout);
-        kefu_layout = (RelativeLayout) view.findViewById(R.id.kefu_layout);
-        feedback_layout = (RelativeLayout) view.findViewById(R.id.feedback_layout);
-        clearCacheLayout = (RelativeLayout) view.findViewById(R.id.clear_cache_layout);
-        cacheSizeView = (TextView) view.findViewById(R.id.cache_size);
-        callPhoneLayout = (RelativeLayout) view.findViewById(R.id.call_layout);
-        commentLayout = (RelativeLayout) view.findViewById(R.id.comment_layout);
-        bindingAccountLayout = (RelativeLayout) view.findViewById(R.id.binding_account_layout);
-        head_img = (ImageView) view.findViewById(R.id.head_img);
-        user_head_img = (ImageView) view.findViewById(R.id.user_head_img);
-        my_account = (TextView) view.findViewById(R.id.frag_my_account);
-        my_name = (TextView) view.findViewById(R.id.frag_my_name);
-
-        commentCountView = (BadgeView) view.findViewById(R.id.comment_count_text);
-        noticeCountView = (BadgeView) view.findViewById(R.id.notify_count_text);
+    public void initView() {
         commentCountView.setVisibility(View.GONE);
         noticeCountView.setVisibility(View.GONE);
-
-
-        scrollView = (ScrollView) view.findViewById(R.id.setting_scrollview);
-
         cacheSizeView.setText(UiHelper.caculateCacheSize());
         //动态计算imageview的宽高
-        head_img.setLayoutParams(new FrameLayout.LayoutParams((int) TDevice.getScreenWidth(), (int) (880 / (1242 / TDevice.getScreenWidth()))));
-
+        head_img.setLayoutParams(new FrameLayout.LayoutParams((int) TDevice.getScreenWidth(), (int) (880 / (1242 /
+                TDevice.getScreenWidth()))));
         getUnReadMessageCount(Constant.searchMsgCountType1, Constant.searchMsgCountType2);
     }
 
@@ -117,18 +133,47 @@ public class MyNewFragment extends BaseFragment implements View.OnClickListener{
         }
     }
 
-    public void setListener() {
-        notifyLayout.setOnClickListener(this);
-        my_collect_layout.setOnClickListener(this);
-        setting_layout.setOnClickListener(this);
-        my_info_layout.setOnClickListener(this);
-        kefu_layout.setOnClickListener(this);
-        feedback_layout.setOnClickListener(this);
-        clearCacheLayout.setOnClickListener(this);
-        callPhoneLayout.setOnClickListener(this);
-        commentLayout.setOnClickListener(this);
-        bindingAccountLayout.setOnClickListener(this);
+    @OnClick({R.id.notify_layout, R.id.collect_layout, R.id.frag_my_info_layout, R.id.kefu_layout, R.id
+            .setting_layout, R.id.feedback_layout, R.id.clear_cache_layout, R.id.call_layout, R.id.comment_layout, R
+            .id.binding_account_layout})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.notify_layout:
+                startActivity(NoticeActivity.class);
+                break;
+            case R.id.collect_layout:
+                startActivity(CollectActivity.class);
+                break;
+            case R.id.frag_my_info_layout:
+                startActivityForResult(UserInfoActivity.class, REQUESTCODE_USERINFO);
+                break;
+            case R.id.kefu_layout:
+                startActivity(CustomerServiceActivity.class);
+                break;
+            case R.id.setting_layout:
+                startActivity(AboutActivity.class);
+                break;
+            case R.id.feedback_layout:
+                startActivity(FeedBackActivity.class);
+                break;
+            case R.id.clear_cache_layout:
+                onClickCleanCache();
+                break;
+            case R.id.call_layout:
+                UiHelper.callPhoneIntent(getContext(), getString(R.string.app_phone));
+                break;
+            case R.id.comment_layout:
+                startActivity(CommentListActivity.class);
+                break;
+            case R.id.binding_account_layout:
+                startActivity(BindingAccountActivity.class);
+                break;
+            default:
+                break;
+        }
+    }
 
+    private void setListener() {
         scrollView.setOverScrollMode(View.OVER_SCROLL_NEVER);
     }
 
@@ -136,7 +181,8 @@ public class MyNewFragment extends BaseFragment implements View.OnClickListener{
     public void onResume() {
         super.onResume();
         initMyInfo();
-        my_name.setText(TextUtils.isEmpty(dataManager.getUserName()) ? getResources().getString(R.string.ower) : dataManager.getUserName());
+        my_name.setText(TextUtils.isEmpty(dataManager.getUserName()) ? getResources().getString(R.string.ower) :
+                dataManager.getUserName());
         my_account.setText(TextUtils.isEmpty(dataManager.getAccount()) ? "" : "账号：" + dataManager.getAccount());
     }
 
@@ -184,44 +230,6 @@ public class MyNewFragment extends BaseFragment implements View.OnClickListener{
         }, this, selectLists);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.notify_layout:
-                startActivity(NoticeActivity.class);
-                break;
-            case R.id.collect_layout:
-                startActivity(CollectActivity.class);
-                break;
-            case R.id.frag_my_info_layout:
-                startActivityForResult(UserInfoActivity.class, REQUESTCODE_USERINFO);
-                break;
-            case R.id.kefu_layout:
-                startActivity(CustomerServiceActivity.class);
-                break;
-            case R.id.setting_layout:
-                startActivity(AboutActivity.class);
-                break;
-            case R.id.feedback_layout:
-                startActivity(FeedBackActivity.class);
-                break;
-            case R.id.clear_cache_layout:
-                onClickCleanCache();
-                break;
-            case R.id.call_layout:
-                UiHelper.callPhoneIntent(getContext(), getString(R.string.app_phone));
-                break;
-            case R.id.comment_layout:
-                startActivity(CommentListActivity.class);
-                break;
-            case R.id.binding_account_layout:
-                startActivity(BindingAccountActivity.class);
-                break;
-            default:
-                break;
-        }
-    }
-
     /**
      * 清空缓存
      */
@@ -246,11 +254,6 @@ public class MyNewFragment extends BaseFragment implements View.OnClickListener{
 
 
     @Override
-    public int getLayoutId() {
-        return R.layout.fragment_my_new;
-    }
-
-    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode != getActivity().RESULT_OK) {
@@ -261,5 +264,10 @@ public class MyNewFragment extends BaseFragment implements View.OnClickListener{
                 initMyInfo();
                 break;
         }
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_my_new;
     }
 }
