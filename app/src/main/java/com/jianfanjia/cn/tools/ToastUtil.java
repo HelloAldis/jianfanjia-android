380bb64d89c10f130ae6d933e0c1c61e9fb62f7b
zhhao226 <352233265@qq.com>
2016-03-29 15:09:49 +0800

    update code

--- app/src/main/java/com/jianfanjia/cn/tools/ToastUtil.java
+++ app/src/main/java/com/jianfanjia/cn/tools/ToastUtil.java
@@ -1,17 +1,18 @@
 package com.jianfanjia.cn.tools;
 
-import android.app.Activity;
+import android.content.Context;
 import android.widget.Toast;
 
 /**
  * Created by Aldis on 16/3/24.
  */
 public class ToastUtil {
-    public void showShortTost(Activity activity, String text) {
-        Toast.makeText(activity, text, Toast.LENGTH_SHORT).show();
+
+    public static void showShortTost(Context context, String text) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
     }
 
-    public void showLongTost(Activity activity, String text) {
-        Toast.makeText(activity, text, Toast.LENGTH_LONG).show();
+    public static void showLongTost(Context context, String text) {
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show();
     }
 }
