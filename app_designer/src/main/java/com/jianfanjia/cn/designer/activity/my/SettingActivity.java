26940249edf7bd5fc6f49f8e486d93c595b7bb38
feng0413 <041332feng@163.com>
2016-03-30 19:51:10 +0800

    SettingActivity.java

--- app_designer/src/main/java/com/jianfanjia/cn/designer/activity/my/SettingActivity.java
+++ app_designer/src/main/java/com/jianfanjia/cn/designer/activity/my/SettingActivity.java
@@ -3,7 +3,6 @@ package com.jianfanjia.cn.designer.activity.my;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.RelativeLayout;
 
 import com.jianfanjia.cn.designer.R;
@@ -19,59 +18,52 @@ import com.jianfanjia.cn.designer.view.dialog.CommonDialog;
 import com.jianfanjia.cn.designer.view.dialog.DialogHelper;
 import com.umeng.socialize.bean.SHARE_MEDIA;
 
+import org.androidannotations.annotations.Click;
+
+import butterknife.Bind;
+
 /**
  * Description:设置
  * Author：fengliang
  * Email：leo.feng@myjyz.com
  * Date:15-10-11 14:30
  */
-public class SettingActivity extends BaseActivity implements OnClickListener {
+public class SettingActivity extends BaseActivity {
     private static final String TAG = SettingActivity.class.getName();
-    private RelativeLayout aboutFragment = null;
-    private RelativeLayout logoutLayout = null;
-    private RelativeLayout helpLayout = null;
-    private MainHeadView mainHeadView = null;
+
+    @Bind(R.id.help_layout)
+    RelativeLayout aboutFragment;
+
+    @Bind(R.id.logout_layout)
+    RelativeLayout logoutLayout;
+
+    @Bind(R.id.my_setting_head_layout)
+    MainHeadView mainHeadView;
+
     private ShareUtil shareUtil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         shareUtil = new ShareUtil(this);
+        initView();
     }
 
-    @Override
-    public void initView() {
+    private void initView() {
         initMainHeadView();
-        helpLayout = (RelativeLayout) findViewById(R.id.help_layout);
-        aboutFragment = (RelativeLayout) findViewById(R.id.about_layout);
-        logoutLayout = (RelativeLayout) findViewById(R.id.logout_layout);
-
     }
 
     private void initMainHeadView() {
-        mainHeadView = (MainHeadView) findViewById(R.id.my_setting_head_layout);
-        mainHeadView.setBackListener(this);
-        mainHeadView
-                .setMianTitle(getResources().getString(R.string.my_setting));
+        mainHeadView.setMianTitle(getResources().getString(R.string.my_setting));
         mainHeadView.setLayoutBackground(R.color.head_layout_bg);
         mainHeadView.setDividerVisable(View.VISIBLE);
     }
 
-    @Override
-    public void setListener() {
-        aboutFragment.setOnClickListener(this);
-        helpLayout.setOnClickListener(this);
-        logoutLayout.setOnClickListener(this);
-    }
-
-    @Override
+    @Click({R.id.about_layout, R.id.logout_layout, R.id.head_back_layout})
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.about_layout:
                 startActivity(AboutActivity.class);
-                break;
-            case R.id.help_layout:
-                startActivity(HelpActivity.class);
                 break;
             case R.id.logout_layout:
                 onClickExit();
@@ -79,7 +71,6 @@ public class SettingActivity extends BaseActivity implements OnClickListener {
             case R.id.head_back_layout:
                 appManager.finishActivity(this);
                 break;
-
             default:
                 break;
         }
@@ -100,7 +91,8 @@ public class SettingActivity extends BaseActivity implements OnClickListener {
                         dataManager.cleanData();
                         MyApplication.getInstance().clearCookie();
                         appManager.finishAllActivity();
-                        AuthUtil.getInstance(SettingActivity.this).deleteOauth(SettingActivity.this, SHARE_MEDIA.WEIXIN);
+                        AuthUtil.getInstance(SettingActivity.this).deleteOauth(SettingActivity.this, SHARE_MEDIA
+                                .WEIXIN);
                         startActivity(LoginNewActivity_.class);
                         finish();
                     }
