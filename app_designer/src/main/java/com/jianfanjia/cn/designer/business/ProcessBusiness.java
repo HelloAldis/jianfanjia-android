52e4f7edde9d14e7999c196d1aa0af9c4d82f65a
zhhao226 <352233265@qq.com>
2016-03-31 11:32:33 +0800

    update code

---
+++ app_designer/src/main/java/com/jianfanjia/cn/designer/business/ProcessBusiness.java
@@ -0,0 +1,12 @@
+package com.jianfanjia.cn.designer.business;
+
+/**
+ * Description: com.jianfanjia.cn.designer.business
+ * Author: zhanghao
+ * Email: jame.zhang@myjyz.com
+ * Date:2016-03-31 11:29
+ */
+public class ProcessBusiness {
+
+
+}
