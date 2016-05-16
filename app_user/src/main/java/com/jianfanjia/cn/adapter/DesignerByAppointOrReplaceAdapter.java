52da514aa3b9fdc5c68bb2fad0dd12e465fdb3cb
zhhao226 <352233265@qq.com>
2016-05-16 16:30:12 +0800

    update code

--- app_user/src/main/java/com/jianfanjia/cn/adapter/DesignerByAppointOrReplaceAdapter.java
+++ app_user/src/main/java/com/jianfanjia/cn/adapter/DesignerByAppointOrReplaceAdapter.java
@@ -102,7 +102,7 @@ public class DesignerByAppointOrReplaceAdapter extends BaseRecyclerViewAdapter<M
                     holder.itemInfoAuthView.setVisibility(View.GONE);
                 }
                 List<String> tags = designerCanOrderInfo.getTags();
-                if(tags.size() > 0){
+                if(tags != null && tags.size() > 0){
                     holder.itemTagText.setVisibility(View.VISIBLE);
                     holder.itemTagText.setText(tags.get(0));
                     switch (tags.get(0)){
