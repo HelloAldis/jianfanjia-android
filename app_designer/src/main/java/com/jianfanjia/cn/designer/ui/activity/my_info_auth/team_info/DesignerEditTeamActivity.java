4c77098d73cb514e38dcc1626401f541f00e57b7
zhhao226 <352233265@qq.com>
2016-05-25 20:28:35 +0800

    update copde

---
+++ app_designer/src/main/java/com/jianfanjia/cn/designer/ui/activity/my_info_auth/team_info/DesignerEditTeamActivity.java
@@ -0,0 +1,18 @@
+package com.jianfanjia.cn.designer.ui.activity.my_info_auth.team_info;
+
+import com.jianfanjia.cn.designer.R;
+import com.jianfanjia.cn.designer.base.BaseSwipeBackActivity;
+
+/**
+ * Description: com.jianfanjia.cn.designer.ui.activity.my_info_auth.team_info
+ * Author: zhanghao
+ * Email: jame.zhang@myjyz.com
+ * Date:2016-05-25 20:13
+ */
+public class DesignerEditTeamActivity extends BaseSwipeBackActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_designer_auth_edit_team;
+    }
+}
