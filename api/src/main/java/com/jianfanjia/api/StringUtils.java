2b99ad8263208cff30b9386b9b677787231a655b
zhhao226 <352233265@qq.com>
2016-06-30 15:07:12 +0800

    update code

--- api/src/main/java/com/jianfanjia/api/StringUtils.java
+++ api/src/main/java/com/jianfanjia/api/StringUtils.java
@@ -2,8 +2,6 @@ package com.jianfanjia.api;
 
 import android.text.TextUtils;
 
-import com.jianfanjia.common.tool.LogTool;
-
 /**
  * Description: com.jianfanjia.api
  * Author: zhanghao
@@ -14,7 +12,6 @@ public class StringUtils {
 
     public static String unEscapeHtmp(String src) {
         if (TextUtils.isEmpty(src)) return null;
-        LogTool.d(StringUtils.class.getName(), src);
         if (src.contains("&quot;")) {
             src = src.replace("&quot;", "\"");
         }
